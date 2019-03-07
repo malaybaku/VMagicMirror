fd7b00ed9a98c29846f403f7ed410b171269f6b7
malaybaku <njslyr.trpg+harriet@gmail.com>
2019-03-07 23:02:37 +0900

    import project without UniVRM related assets

---
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/BackgroundColorController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Baku.VMagicMirror
+{
+    [RequireComponent(typeof(Camera))]
+    public class BackgroundColorController : MonoBehaviour
+    {
+        private Camera _camera;
+
+        void Start()
+        {
+            _camera = GetComponent<Camera>();
+        }
+
+        public void ChangeColor(float a, float r, float g, float b)
+        {
+            _camera.backgroundColor = new Color(r, g, b, a);
+        }
+    }
+}
+

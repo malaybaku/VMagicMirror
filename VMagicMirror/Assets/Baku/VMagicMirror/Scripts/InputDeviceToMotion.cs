e837d58a600dfda0722b4eb32204a01843f6457d
malaybaku <njslyr.trpg+harriet@gmail.com>
2019-03-10 01:51:22 +0900

    Add VRM loading logic except Final IK related code

---
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/InputDeviceToMotion.cs
@@ -0,0 +1,233 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Baku.VMagicMirror
+{
+    public class InputDeviceToMotion : MonoBehaviour
+    {
+        #region settings 
+
+        [SerializeField]
+        private KeyboardProvider keyboard = null;
+
+        [SerializeField]
+        private TouchPadProvider touchPad = null;
+
+        [SerializeField]
+        private Transform leftHandTarget = null;
+
+        [SerializeField]
+        private Transform rightHandTarget = null;
+
+        [SerializeField]
+        private Transform headTarget = null;
+
+        [SerializeField]
+        private AnimationCurve keyboardHorizontalApproachCurve = new AnimationCurve(new Keyframe[]
+        {
+            new Keyframe(0.0f, 1, -1, -1),
+            new Keyframe(0.5f, 0, -1, 0),
+            new Keyframe(1.0f, 0, 0, 0),
+        });
+
+        [SerializeField]
+        private AnimationCurve keyboardVerticalApproachCurve = new AnimationCurve(new Keyframe[]
+        {
+            new Keyframe(0.0f, 1, 0, 0),
+            new Keyframe(0.5f, 0, -1, 1),
+            new Keyframe(1.0f, 1, 0, 0),
+        });
+
+        //高さ方向についてはキー連打時に手が強引な動きに見えないよう、二重に重みづけする
+        [SerializeField]
+        private AnimationCurve keyboardVerticalWeightCurve = new AnimationCurve(new Keyframe[]
+        {
+            new Keyframe(0, 0.2f),
+            new Keyframe(0.5f, 1.0f),
+        });
+
+        [SerializeField]
+        private float yOffsetAlways = 0.05f;
+
+        private Vector3 yOffsetAlwaysVec => yOffsetAlways * Vector3.up;
+
+        [SerializeField]
+        private float yOffsetAfterKeyDown = 0.08f;
+
+        [SerializeField]
+        private float keyboardMotionDuration = 0.25f;
+
+        [SerializeField]
+        private float touchPadApproachSpeedFactor = 0.2f;
+
+        [SerializeField]
+        private float headTargetMoveSpeedFactor = 0.2f;
+
+        [SerializeField]
+        private AnimationCurve clickHandRotationAnimation = new AnimationCurve(new Keyframe[]
+        {
+            new Keyframe(0, 0, 0, 1),
+            new Keyframe(0.1f, 10, 1, -1),
+            new Keyframe(0.2f, 0, 0, 0),
+        });
+
+        [SerializeField]
+        private float clickHandRotationDuration = 0.2f;
+
+        //手首をIKすると(指先じゃなくて)手首がキー位置に行ってしまうので、その手首位置を原点方向にずらすための長さ。
+        //できれば決め打ちじゃなくてVRMの読み込み直後に長さを調べてほしい
+        public float handToTipLength = 0.1f;
+
+        //こっちはマウス用
+        public float handToPalmLength = 0.05f;
+
+        //クリック時にクイッとさせたいので。
+        public Transform rightHandBone = null;
+
+        #endregion
+
+        private Coroutine _leftHandMoveCoroutine = null;
+        private Coroutine _rightHandMoveCoroutine = null;
+        private Coroutine _clickMoveCoroutine = null;
+
+        private bool _touchPadTargetEnabled = false;
+        private Vector3 _touchPadTargetPosition = Vector3.zero;
+        private Transform _headTransformToTrack = null;
+
+        private void Update()
+        {
+            if (_touchPadTargetEnabled)
+            {
+                rightHandTarget.position = Vector3.Lerp(
+                    rightHandTarget.position,
+                    _touchPadTargetPosition,
+                    touchPadApproachSpeedFactor
+                    );
+            }
+
+            if (_headTransformToTrack != null)
+            {
+                headTarget.position = Vector3.Lerp(
+                    headTarget.position,
+                    _headTransformToTrack.position,
+                    headTargetMoveSpeedFactor
+                    );
+            }
+        }
+
+        public void PressKeyMotion(string key)
+        {
+            Vector3 targetPos = keyboard.GetPositionOfKey(key) + yOffsetAlwaysVec;
+            targetPos -= handToTipLength * new Vector3(targetPos.x, 0, targetPos.z).normalized;
+
+            if (keyboard.IsLeftHandPreffered(key))
+            {
+                if (_leftHandMoveCoroutine != null)
+                {
+                    StopCoroutine(_leftHandMoveCoroutine);
+                }
+                _leftHandMoveCoroutine = StartCoroutine(MoveTargetToKeyboard(leftHandTarget, targetPos));
+                _headTransformToTrack = leftHandTarget;
+            }
+            else
+            {
+                _touchPadTargetEnabled = false;
+                if (_rightHandMoveCoroutine != null)
+                {
+                    StopCoroutine(_rightHandMoveCoroutine);
+                }
+                _rightHandMoveCoroutine = StartCoroutine(MoveTargetToKeyboard(rightHandTarget, targetPos));
+                _headTransformToTrack = rightHandTarget;
+            }
+        }
+
+        public void GrabMouseMotion(int x, int y)
+        {
+            float xClamped = Mathf.Clamp(x - Screen.width * 0.5f, -1000, 1000) / 1000.0f;
+            float yClamped = Mathf.Clamp(y - Screen.height * 0.5f , -1000, 1000) / 1000.0f;
+            var targetPos = touchPad.GetHandTipPosFromScreenPoint(xClamped, yClamped) + yOffsetAlwaysVec;
+            targetPos -= handToPalmLength * new Vector3(targetPos.x, 0, targetPos.z).normalized;
+
+
+            if (_rightHandMoveCoroutine != null)
+            {
+                StopCoroutine(_rightHandMoveCoroutine);
+            }
+            _touchPadTargetPosition = targetPos;
+            _touchPadTargetEnabled = true;
+
+            _headTransformToTrack = rightHandTarget;
+        }
+
+        public void ClickMotion(string info)
+        {
+            if (_clickMoveCoroutine != null)
+            {
+                StopCoroutine(_clickMoveCoroutine);
+            }
+
+            _clickMoveCoroutine = StartCoroutine(ClickMotionByRightHand());
+        }
+
+        private IEnumerator MoveTargetToKeyboard(Transform t, Vector3 targetPos)
+        {
+            float startTime = Time.time;
+            Vector3 startPos = t.position;
+
+            while (Time.time - startTime < keyboardMotionDuration)
+            {
+                float rate = (Time.time - startTime) / keyboardMotionDuration;
+
+                Vector3 horizontal = Vector3.Lerp(startPos, targetPos, keyboardHorizontalApproachCurve.Evaluate(rate));
+
+                if (rate < 0.5f)
+                {
+                    //アプローチ中: yのカーブに重みを付けつつ近づく
+              
+                    float verticalTarget = Mathf.Lerp(startPos.y, targetPos.y, keyboardVerticalApproachCurve.Evaluate(rate));
+                    float vertical = Mathf.Lerp(t.position.y, verticalTarget, keyboardVerticalWeightCurve.Evaluate(rate));
+                    t.position = new Vector3(horizontal.x, vertical, horizontal.z);
+                }
+                else
+                {
+                    //離れるとき: yを引き上げる。気持ち的には(targetPos.y + yOffset)がスタート位置側にあたるので、ウェイトを1から0に引き戻す感じ
+                    float verticalTarget = Mathf.Lerp(targetPos.y + yOffsetAfterKeyDown, targetPos.y, keyboardVerticalApproachCurve.Evaluate(rate));
+                    t.position = new Vector3(horizontal.x, verticalTarget, horizontal.z);
+                }
+
+                yield return null;
+            }
+
+            //ターゲットを動かすやつはいないので不要…なはず…
+            //var finalTarget = new Vector3(targetPos.x, targetPos.y + yOffset, targetPos.z);
+            //while (true)
+            //{
+            //    t.position = finalTarget;
+            //    yield return null;
+            //}
+        }
+
+        private IEnumerator ClickMotionByRightHand()
+        {
+            if (rightHandBone == null)
+            {
+                yield break;
+            }
+
+            float startTime = Time.time;
+            while (Time.time - startTime < clickHandRotationDuration)
+            {
+                rightHandBone.localRotation = Quaternion.Euler(
+                    0,
+                    0, 
+                    clickHandRotationAnimation.Evaluate(Time.time - startTime)
+                    );
+                yield return null;
+            }
+
+            //note: 無くてもいいかも
+            rightHandBone.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+
+    }
+}
---
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/VRMLoadController.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.IO;
+using UniHumanoid;
+using UnityEngine;
+using UnityEngine.UI;
+using VRM;
+
+namespace Baku.VMagicMirror
+{
+    public class VRMLoadController : MonoBehaviour
+    {
+        [SerializeField]
+        HumanPoseTransfer m_src;
+
+        //[SerializeField]
+        //GameObject m_target;
+
+        [SerializeField]
+        GameObject Root;
+
+        [Serializable]
+        struct TextFields
+        {
+            [SerializeField, Header("Info")]
+            Text m_textModelTitle;
+            [SerializeField]
+            Text m_textModelVersion;
+            [SerializeField]
+            Text m_textModelAuthor;
+            [SerializeField]
+            Text m_textModelContact;
+            [SerializeField]
+            Text m_textModelReference;
+            [SerializeField]
+            RawImage m_thumbnail;
+
+            [SerializeField, Header("CharacterPermission")]
+            Text m_textPermissionAllowed;
+            [SerializeField]
+            Text m_textPermissionViolent;
+            [SerializeField]
+            Text m_textPermissionSexual;
+            [SerializeField]
+            Text m_textPermissionCommercial;
+            [SerializeField]
+            Text m_textPermissionOther;
+
+            [SerializeField, Header("DistributionLicense")]
+            Text m_textDistributionLicense;
+            [SerializeField]
+            Text m_textDistributionOther;
+
+            public void Start()
+            {
+                m_textModelTitle.text = "";
+                m_textModelVersion.text = "";
+                m_textModelAuthor.text = "";
+                m_textModelContact.text = "";
+                m_textModelReference.text = "";
+
+                m_textPermissionAllowed.text = "";
+                m_textPermissionViolent.text = "";
+                m_textPermissionSexual.text = "";
+                m_textPermissionCommercial.text = "";
+                m_textPermissionOther.text = "";
+
+                m_textDistributionLicense.text = "";
+                m_textDistributionOther.text = "";
+            }
+
+            public void UpdateMeta(VRMImporterContext context)
+            {
+                var meta = context.ReadMeta(true);
+
+                m_textModelTitle.text = meta.Title;
+                m_textModelVersion.text = meta.Version;
+                m_textModelAuthor.text = meta.Author;
+                m_textModelContact.text = meta.ContactInformation;
+                m_textModelReference.text = meta.Reference;
+
+                m_textPermissionAllowed.text = meta.AllowedUser.ToString();
+                m_textPermissionViolent.text = meta.ViolentUssage.ToString();
+                m_textPermissionSexual.text = meta.SexualUssage.ToString();
+                m_textPermissionCommercial.text = meta.CommercialUssage.ToString();
+                m_textPermissionOther.text = meta.OtherPermissionUrl;
+
+                m_textDistributionLicense.text = meta.LicenseType.ToString();
+                m_textDistributionOther.text = meta.OtherLicenseUrl;
+
+                m_thumbnail.texture = meta.Thumbnail;
+            }
+        }
+
+        [SerializeField]
+        TextFields m_texts;
+
+        [SerializeField]
+        InputDeviceToMotion _inputToMotion;
+
+        //TODO: この辺はちょっと分けたい気がしないでもないが。
+        [SerializeField]
+        RuntimeAnimatorController runtimeController;
+        [SerializeField]
+        Transform rightHand;
+        [SerializeField]
+        Transform leftHand;
+        [SerializeField]
+        Transform head;
+
+        HumanPoseTransfer m_loaded;
+
+        private void Start()
+        {
+            m_texts.Start();
+        }
+
+        private void Update()
+        {
+            //note: ここでやらんでWPFの指示でオンオフすべきでは
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                Root?.SetActive(!Root.activeSelf);
+            }
+        }
+
+        public void LoadModelOnlyForPreview(string path)
+        {
+            if (!File.Exists(path))
+            {
+                return;
+            }
+
+            Debug.LogFormat("{0}", path);
+            var ext = Path.GetExtension(path).ToLower();
+            switch (ext)
+            {
+                case ".vrm":
+                    using (var context = new VRMImporterContext())
+                    {
+                        context.ParseGlb(File.ReadAllBytes(path));
+                        m_texts.UpdateMeta(context);
+                    }
+                    break;
+                default:
+                    Debug.LogWarningFormat("unknown file type: {0}", path);
+                    break;
+            }
+        }
+
+        public void LoadModel(string path)
+        {
+            if (!File.Exists(path))
+            {
+                return;
+            }
+
+            Debug.LogFormat("{0}", path);
+            var ext = Path.GetExtension(path).ToLower();
+            switch (ext)
+            {
+                case ".vrm":
+                    {
+                        var context = new VRMImporterContext();
+                        var file = File.ReadAllBytes(path);
+                        context.ParseGlb(file);
+                        m_texts.UpdateMeta(context);
+                        context.Load();
+                        context.ShowMeshes();
+                        context.EnableUpdateWhenOffscreen();
+                        context.ShowMeshes();
+                        SetModel(context.Root);
+                        break;
+                    }
+
+                //case ".glb":
+                //    {
+                //        var context = new UniGLTF.ImporterContext();
+                //        var file = File.ReadAllBytes(path);
+                //        context.ParseGlb(file);
+                //        context.Load();
+                //        context.ShowMeshes();
+                //        context.EnableUpdateWhenOffscreen();
+                //        context.ShowMeshes();
+                //        SetModel(context.Root);
+                //        break;
+                //    }
+
+                default:
+                    Debug.LogWarningFormat("unknown file type: {0}", path);
+                    break;
+            }
+        }
+
+        void SetModel(GameObject go)
+        {
+            // cleanup
+            var loaded = m_loaded;
+            m_loaded = null;
+
+            if (loaded != null)
+            {
+                Debug.LogFormat("destroy {0}", loaded);
+                GameObject.Destroy(loaded.gameObject);
+            }
+
+            if (go != null)
+            {
+                var lookAt = go.GetComponent<VRMLookAtHead>();
+                if (lookAt != null)
+                {
+                    m_loaded = go.AddComponent<HumanPoseTransfer>();
+                    m_loaded.Source = m_src;
+                    m_loaded.SourceType = HumanPoseTransfer.HumanPoseTransferSourceType.HumanPoseTransfer;
+                }
+
+                var animation = go.GetComponent<Animation>();
+                if (animation && animation.clip != null)
+                {
+                    animation.Play(animation.clip.name);
+                }
+
+                //セットアップの過程でFinalIKに少し触るので(規約クリアになるよう)ファイルを分離
+                VRMLoadControllerHelper.SetupVrm(go, new VRMLoadControllerHelper.VrmLoadSetting()
+                {
+                    runtimeAnimatorController = runtimeController,
+                    leftHandTarget = leftHand,
+                    rightHandTarget = rightHand,
+                    headTarget = head,
+                    inputToMotion = _inputToMotion,
+                });
+            }
+        }
+    }
+}

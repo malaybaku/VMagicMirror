13117932fa6e02217ee256cf92bf258b409b4762
malaybaku <njslyr.trpg+harriet@gmail.com>
2019-03-11 23:23:15 +0900

    classify scripts / setup build setting

---
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Interprocess/WpfActivator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace Baku.VMagicMirror
+{
+    public class WpfActivator : MonoBehaviour
+    {
+        private static readonly string ConfigExePath = "..\\ConfigApp\\VMagicMirrorConfig.exe";
+
+        private static string GetWpfPath() 
+            => Path.Combine(
+                Path.GetDirectoryName(Assembly.GetEntryAssembly().Location),
+                ConfigExePath
+                );
+
+        void Start()
+        {
+            //Startが全部終わって落ち着いた状態でロードしたいので遅延付ける
+            StartCoroutine(ActivateWpf());
+        }
+
+        private IEnumerator ActivateWpf()
+        {
+            string path = GetWpfPath();
+
+            yield return new WaitForSeconds(1.0f);
+
+            if (File.Exists(path))
+            {
+                Process.Start(path);
+            }
+        }
+    }
+}

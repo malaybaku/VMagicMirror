6745cf6e644fed51d77b3e750c0363caa16fde07
malaybaku <njslyr.trpg+harriet@gmail.com>
2019-03-19 22:44:22 +0900

    receiver to toggle lip sync on / off

---
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Interprocess/LipSyncController.cs
@@ -0,0 +1,21 @@
+﻿using AniLipSync.VRM;
+using UnityEngine;
+
+namespace Baku.VMagicMirror
+{
+    [RequireComponent(typeof(AnimMorphTarget))]
+    public class LipSyncController : MonoBehaviour
+    {
+        private AnimMorphTarget _animMorphTarget = null;
+
+        private void Start()
+        {
+            _animMorphTarget = GetComponent<AnimMorphTarget>();
+        }
+
+        public void SetLipSyncEnable(bool isEnabled)
+        {
+            _animMorphTarget.enabled = isEnabled;
+        }
+    }
+}

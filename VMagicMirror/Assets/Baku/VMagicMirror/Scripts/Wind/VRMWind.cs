8660c009216b801b15f0094c5df958b513d7583b
malaybaku <njslyr.trpg+harriet@gmail.com>
2019-11-04 21:58:26 +0900

    (wip) wind impl with fully fixed parameters

---
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Wind/VRMWind.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using VRM;
+using Zenject;
+
+namespace Baku.VMagicMirror
+{
+    /// <summary>
+    /// VRMの揺れものに対して風を発生させる発生源の処理
+    /// </summary>
+    public class VRMWind : MonoBehaviour
+    {
+        [SerializeField] private bool enableWind = false;
+        
+        //風の基本の方向と、方向をランダムでズラすファクタ
+        [SerializeField] private Vector3 windBaseOrientation = Vector3.left;
+        [SerializeField] private float windOrientationRandomPower = 0.2f;
+
+        //風の発生頻度、強さ、立ち上がりと立ち下がりの時間を、それぞれ全てRandom.Rangeに通すために幅付きの値にする
+        [SerializeField] private Vector2 windIntervalRange = new Vector2(0.55f, 1.25f);
+        [SerializeField] private Vector2 windStrengthRange = new Vector2(0.03f, 0.06f);
+        [SerializeField] private Vector2 windRiseCountRange = new Vector2(0.4f, 0.6f);
+        [SerializeField] private Vector2 windSitCountRange = new Vector2(0.9f, 1.1f);
+
+        [Inject] private IVRMLoadable _vrmLoadController;
+        
+        class WindItem
+        {
+            public WindItem(Vector3 orientation, float riseCount, float sitCount, float maxFactor)
+            {
+                Orientation = orientation;
+                RiseCount = riseCount;
+                SitCount = sitCount;
+                MaxFactor = maxFactor;
+
+                TotalTime = RiseCount + SitCount;
+            } 
+            
+            public Vector3 Orientation { get; }
+            public float RiseCount { get; }
+            public float SitCount { get; }
+            public float MaxFactor { get; }
+            public float TotalTime { get; }
+            public float TimeCount { get; set; }
+
+            public float CurrentFactor =>
+                TimeCount < RiseCount
+                ? MaxFactor * TimeCount / RiseCount
+                : MaxFactor * (1 - (TimeCount - RiseCount) / SitCount);
+        }
+        
+        private float _windGenerateCount = 0;
+        private VRMSpringBone[] _springBones = new VRMSpringBone[] { };
+        private Vector3[] _originalGravityDirections = new Vector3[] { };
+        private float[] _originalGravityFactors = new float[] { };
+        private readonly List<WindItem> _windItems = new List<WindItem>();
+
+        public bool WindEnabled { get; private set; }
+
+        public void EnableWind(bool enable)
+        {
+            if (WindEnabled != enable)
+            {
+                WindEnabled = enable;
+                if (!WindEnabled)
+                {
+                    DisableWind();
+                }
+            }
+        }
+
+        private void DisableWind()
+        {
+            for (int i = 0; i < _springBones.Length; i++)
+            {
+                var bone = _springBones[i];
+                bone.m_gravityDir = _originalGravityDirections[i];
+                bone.m_gravityPower = _originalGravityFactors[i];
+            }
+        }
+
+        private void Start()
+        {
+            _vrmLoadController.VrmLoaded += OnVrmLoaded;
+            _vrmLoadController.VrmDisposing += OnVrmUnloading;
+        }
+
+        private void Update()
+        {
+            EnableWind(enableWind);
+            if (!WindEnabled)
+            {
+                return;
+            }
+
+            UpdateWindGenerateCount();
+            UpdateWindItems();
+            
+            Vector3 localWindForce = Vector3.zero;
+            for (int i = 0; i < _windItems.Count; i++)
+            {
+                localWindForce += _windItems[i].CurrentFactor * _windItems[i].Orientation;
+            }
+
+            Vector3 windForce = transform.rotation * localWindForce;
+            for (int i = 0; i < _springBones.Length; i++)
+            {
+                var bone = _springBones[i];
+                //NOTE: 力を合成して斜めに力をかけるのが狙い
+                var forceSum = _originalGravityFactors[i] * _originalGravityDirections[i] + windForce;
+                bone.m_gravityDir = forceSum.normalized;
+                bone.m_gravityPower = forceSum.magnitude;
+            }
+        }
+
+        private void UpdateWindGenerateCount()
+        {
+            _windGenerateCount -= Time.deltaTime;
+            if (_windGenerateCount > 0)
+            {
+                return;
+            }
+            _windGenerateCount = Random.Range(windIntervalRange.x, windIntervalRange.y);
+
+            var windOrientation = (
+                windBaseOrientation + 
+                new Vector3(
+                   Random.Range(-windOrientationRandomPower, windOrientationRandomPower),
+                   Random.Range(-windOrientationRandomPower, windOrientationRandomPower),
+                   Random.Range(-windOrientationRandomPower, windOrientationRandomPower)
+                    )).normalized;
+            
+            _windItems.Add(new WindItem(
+                windOrientation,    
+                Random.Range(windRiseCountRange.x, windRiseCountRange.y),
+                Random.Range(windSitCountRange.x, windSitCountRange.y),
+                Random.Range(windStrengthRange.x, windStrengthRange.y)
+            ));
+        }
+
+        private void UpdateWindItems()
+        {
+            //Removeする可能性があるので逆順にやってます
+            for (int i = _windItems.Count - 1; i >= 0; i--)
+            {
+                var item = _windItems[i];
+                item.TimeCount += Time.deltaTime;
+                if (item.TimeCount >= item.TotalTime)
+                {
+                    _windItems.RemoveAt(i);
+                }
+            }
+        }
+        
+        private void OnVrmUnloading()
+        {
+            _springBones = new VRMSpringBone[] { };
+            _originalGravityDirections = new Vector3[]{ };
+            _originalGravityFactors = new float[] { };
+        }
+
+        private void OnVrmLoaded(VrmLoadedInfo info)
+        {
+            _springBones = info.vrmRoot.GetComponentsInChildren<VRMSpringBone>();
+            _originalGravityDirections = _springBones.Select(b => b.m_gravityDir).ToArray();
+            _originalGravityFactors = _springBones.Select(b => b.m_gravityPower).ToArray();
+        }
+    }
+}

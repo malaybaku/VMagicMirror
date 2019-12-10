3aa0b6c302f755f0ded6e464681e26b833c1f974
malaybaku <njslyr.trpg+harriet@gmail.com>
2019-12-11 01:52:08 +0900

    remove unused messages

--- VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Interprocess/Model/MessageCommandNames.cs
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Interprocess/Model/MessageCommandNames.cs
@@ -112,12 +112,7 @@
         public const string GamepadLeanMode = nameof(GamepadLeanMode);
         public const string GamepadLeanReverseHorizontal = nameof(GamepadLeanReverseHorizontal);
         public const string GamepadLeanReverseVertical = nameof(GamepadLeanReverseVertical);
-
-        //Layout, Free Layout
-        public const string EnableCustomDevicePositions = nameof(EnableCustomDevicePositions);
-        public const string SetCustomDevicePositions = nameof(SetCustomDevicePositions);
-        public const string EnableFreeDevicesMode = nameof(EnableFreeDevicesMode);
-
+        
         // Lighting 
         public const string LightIntensity = nameof(LightIntensity);
         public const string LightColor = nameof(LightColor);

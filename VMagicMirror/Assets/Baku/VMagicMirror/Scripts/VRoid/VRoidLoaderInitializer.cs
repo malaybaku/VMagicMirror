446075c2b782b36deafa71750e99e80adb0c228c
malaybaku <njslyr.trpg+harriet@gmail.com>
2020-04-28 01:28:38 +0900

    add vroid loader process with dummy prefab

---
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/VRoid/VRoidLoaderInitializer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Baku.VMagicMirror
+{
+    /// <summary>
+    /// VRoidのロード処理を行うオブジェクトをInstantiateするだけのクラス。
+    /// </summary>
+    /// <remarks>
+    /// ここのtargetPrefabにVRoidのクライアント実装が入りますが、レポジトリ上には本実装を含むprefabはありません。
+    /// (SDKの利用ルールに沿う一環としてSDK周辺のコードをレポジトリから外してます)
+    /// </remarks>
+    public class VRoidLoaderInitializer : MonoBehaviour
+    {
+        [SerializeField] private GameObject targetPrefab = null;
+        
+        private void Start()
+        {
+            if (targetPrefab != null)
+            {
+                Instantiate(targetPrefab);
+            }
+        }
+    }
+}

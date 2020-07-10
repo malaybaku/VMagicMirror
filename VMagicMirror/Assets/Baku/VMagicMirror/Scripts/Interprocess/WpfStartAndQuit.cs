53f4032cc121ba14b485687d3d771ed37194fca2
malaybaku <njslyr.trpg+harriet@gmail.com>
2020-07-11 01:06:17 +0900

    non main thread cleanup system prepare

--- VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Interprocess/WpfStartAndQuit.cs
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Interprocess/WpfStartAndQuit.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
+using System.Threading.Tasks;
 using Zenject;
 
 namespace Baku.VMagicMirror
 {
     public class WpfStartAndQuit : MonoBehaviour
     {
-        [Inject] private IMessageSender sender = null;
-        
         private static readonly string ConfigExePath = "ConfigApp\\VMagicMirrorConfig.exe";
 
         private static string GetWpfPath()
@@ -17,17 +18,62 @@ namespace Baku.VMagicMirror
                 ConfigExePath
                 );
 
-        void Start()
+        private IMessageSender _sender = null;
+        private List<IReleaseBeforeQuit> _releaseItems = new List<IReleaseBeforeQuit>();
+
+        private readonly Atomic<bool> _releaseRunning = new Atomic<bool>();
+        private readonly Atomic<bool> _releaseCompleted = new Atomic<bool>();
+
+        [Inject]
+        public void Initialize(IMessageSender sender, List<IReleaseBeforeQuit> releaseNeededItems)
+        {
+            _sender = sender;
+            _releaseItems = releaseNeededItems;
+        }
+        
+        private void Start()
         {
             StartCoroutine(ActivateWpf());
+            //NOTE: WPF側はProcess.CloseMainWindowを使ってUnityを閉じようとする。
+            //かつ、Unity側で単体で閉じる方法も今のところはメインウィンドウ閉じのみ。
             Application.wantsToQuit += OnApplicationWantsToQuit;
         }
 
         private bool OnApplicationWantsToQuit()
         {
-            //NOTE: we do not disturb app quit itself, just request config close too.
-            sender?.SendCommand(MessageFactory.Instance.CloseConfigWindow());
-            return true;
+            if (_releaseCompleted.Value)
+            {
+                return true;
+            }
+
+            if (_releaseRunning.Value)
+            {
+                return false;
+            }
+            _releaseRunning.Value = true;
+            
+            _sender?.SendCommand(MessageFactory.Instance.CloseConfigWindow());
+
+            //特にリリースするものがないケース: 本来ありえないんだけど、理屈上はほしいので書いておく
+            if (_releaseItems.Count == 0)
+            {
+                _releaseCompleted.Value = true;
+                _releaseRunning.Value = false;
+                return true;
+            }
+            
+            ReleaseItemsAsync();
+            return false;
+        }
+
+        private async void ReleaseItemsAsync()
+        {
+            await Task.WhenAll(
+                _releaseItems.Select(item => item.ReleaseResources())
+            );
+
+            _releaseCompleted.Value = true;
+            _releaseRunning.Value = false;
         }
 
         private IEnumerator ActivateWpf()

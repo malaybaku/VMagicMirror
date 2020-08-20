897b798830d4329b88da2de6af39da5a95d425f9
malaybaku <njslyr.trpg+harriet@gmail.com>
2020-08-20 21:12:59 +0900

    smaller yaw motion

--- VMagicMirror/Assets/Baku/VMagicMirror/Scripts/AvatarControl/Motion/FaceAttitudeController.cs
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/AvatarControl/Motion/FaceAttitudeController.cs
@@ -59,8 +59,8 @@ namespace Baku.VMagicMirror
         
         //体の回転に反映するとかの都合で首ロールを実際に検出した値より控えめに適用しますよ、というファクター
         private const float HeadRollRateApplyFactor = 0.8f;
-        //こっちの2つは角度の指定。これらの値もbodyが動くことまで加味して調整してます
-        private const float HeadYawRateToDegFactor = 28.00f;
+        //こっちの2つは角度の指定。これらの値もbodyが動くことまで加味して調整
+        private const float HeadYawRateToDegFactor = 16.00f;
         private const float HeadPitchRateToDegFactor = 28.0f;
         
         private const float HeadTotalRotationLimitDeg = 40.0f;

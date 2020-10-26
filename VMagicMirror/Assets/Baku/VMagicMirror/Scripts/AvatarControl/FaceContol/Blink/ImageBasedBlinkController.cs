5896511e2eb30d47b39f816f5b060c5f0b179d6e
malaybaku <njslyr.trpg+harriet@gmail.com>
2020-10-27 01:26:01 +0900

    change webcam based blink calc

--- VMagicMirror/Assets/Baku/VMagicMirror/Scripts/AvatarControl/FaceContol/Blink/ImageBasedBlinkController.cs
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/AvatarControl/FaceContol/Blink/ImageBasedBlinkController.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UniRx;
 using Zenject;
 
 namespace Baku.VMagicMirror
@@ -13,62 +12,106 @@ namespace Baku.VMagicMirror
         public IBlinkSource BlinkSource => _blinkSource;
         
         [Inject] private FaceTracker _faceTracker = null;
-        
-        [Tooltip("ブレンドシェイプを変化させていく速度ファクター")]
-        [SerializeField]
-        private float speedFactor = 12f;
-        
-        [Tooltip("瞬き速度を減速させるダンピング")]
-        [SerializeField]
-        private float speedDumpFactor = 0.85f;
-        
-        [Tooltip("瞬きの基本的なアップデート時間間隔。短いほど追従性がよい")]
-        [SerializeField]
-        private float blinkUpdateTimeScale = 0.2f;
 
-        [Tooltip("この値以上のBlink値だったら目が完全に閉じているものと扱う")]
-        [SerializeField] private float closeThreshold = 0.8f;
-        
-        public float blinkUpdateInterval = 0.3f;
-        private float _count = 0;
+        [SerializeField] private float eyeOpenKeepDiffPer16ms = 0.04f;
+        [SerializeField] private float eyeOpenFromCloseDiffPer16ms = 0.1f;
+        [Tooltip("Blink値がこの値を下から上にまたいだ場合、直ちにまばたき状態にする")]
+        [SerializeField] private float blinkJumpThreshold = 0.35f;
 
-        //TODO: ここ以前にT5_4Vやったときと同じで、閉じ方向と開き方向の速度を変えると良いかも。
-        
-        //一定間隔で_latestな値をコピーしてきた、当面ターゲットとすべきまばたきブレンドシェイプの値
-        private float _leftBlinkTarget = 0f;
-        private float _rightBlinkTarget = 0f;
-        
-        //_current(Left|Right)Blinkをターゲット値に持って行くときの速度。PD制御チックに動かす為に使います
-        private float _leftBlinkSpeed = 0f;
-        private float _rightBlinkSpeed = 0f;
-
-        private float _latestFilteredLeft = 0f;
-        private float _latestFilteredRight = 0f;
+        private bool _isLeftEyeOpening = false;
+        private bool _isRightEyeOpening = false;
 
         private void Update()
         {
-            _count -= Time.deltaTime;
-            if (_count < 0)
+            //画像処理の精度が無限に良ければコレで済むが、そうもいかないのでフィルターとかをかませる
+            //_blinkSource.Left = _faceTracker.EyeOpen.LeftEyeBlink;
+            //_blinkSource.Right = _faceTracker.EyeOpen.RightEyeBlink;
+
+            //コンセプト:
+            // - 目開け付近(blink 0 ~ 0.3とか)で動くうちはそれに低速で追従
+            // - それ以上に遷移する場合、まばたき状態に倒してから調整する。
+            // つまり、基本的に半目状態は認めない。
+
+            //deltaTime == 16msの場合向けにパラメタが決まってるので、そこで抑えておけばOK。
+            //また_faceTracker.EyeOpenは最初から[0, 1]の範囲の値を返してるので、
+            //openDiffとかcloseDiffのせいで値が01を超えてもよい
+            float openKeepDiff = eyeOpenKeepDiffPer16ms * Time.deltaTime * 60f;
+            float openFromCloseDiff = eyeOpenFromCloseDiffPer16ms * Time.deltaTime * 60f;
+            
+            float left = _faceTracker.EyeOpen.LeftEyeBlink;
+
+            // 目の動きは3パターン考えると分かりやすい(書いてある通りだけど)
+            if (left > blinkJumpThreshold)
             {
-                _leftBlinkTarget = _faceTracker.EyeOpen.LeftEyeBlink;
-                _rightBlinkTarget =  _faceTracker.EyeOpen.RightEyeBlink;
-                _count = blinkUpdateInterval;                
+                //とりあえず値が一定以上 -> ただちに閉じる。
+                _blinkSource.Left = 1.0f;
+                _isLeftEyeOpening = false;
+            }
+            else if (_blinkSource.Left > blinkJumpThreshold)
+            {
+                // 目を閉じてたのを徐々に開く: 見た目分かりやすいのでMax側にも幅を作っているが、
+                // 条件的にmax側は実際には使われない
+                _blinkSource.Left = Mathf.Clamp(
+                    left, _blinkSource.Left - openFromCloseDiff, _blinkSource.Right + openFromCloseDiff
+                );
+                _isLeftEyeOpening = true;
+            }
+            else if (_isLeftEyeOpening)
+            {
+                // 閉じ目を開く動作の途中: 開く方向には高速で動かすことができる
+                float nextLeft = Mathf.Clamp(
+                    left, _blinkSource.Left - openFromCloseDiff, _blinkSource.Right + openKeepDiff
+                );
+
+                //最高速度で目を開く必要がなくなった = 目開き動作が終わったとみなし、より低速の動きに切り替える
+                if (nextLeft > _blinkSource.Left - openFromCloseDiff)
+                {
+                    _isLeftEyeOpening = false;
+                }
+                
+                _blinkSource.Left = nextLeft;
+            }
+            else
+            {
+                // 目が最初から開いてたのが引き続き開いている
+                _blinkSource.Left = Mathf.Clamp(
+                    left, _blinkSource.Left - openKeepDiff, _blinkSource.Left + openKeepDiff
+                );
             }
 
-            float leftSpeed = (_leftBlinkTarget - _latestFilteredLeft) / blinkUpdateTimeScale;
-            float rightSpeed = (_rightBlinkTarget - _latestFilteredRight) / blinkUpdateTimeScale;
+            float right = _faceTracker.EyeOpen.RightEyeBlink;
+            if (right > blinkJumpThreshold)
+            {
+                //とりあえず値が一定以上 -> ただちに閉じる。
+                _blinkSource.Right = 1.0f;
+            }
+            else if (_blinkSource.Right > blinkJumpThreshold)
+            {
+                // 目を閉じてたのを徐々に開く: 見た目分かりやすいのでMax側にも幅を作っているが、
+                // 条件的にmax側は実際には使われない
+                _blinkSource.Right = Mathf.Clamp(
+                    right, _blinkSource.Right - openFromCloseDiff, _blinkSource.Right + openFromCloseDiff
+                );
+            }
+            else if (_isRightEyeOpening)
+            {
+                float nextRight = Mathf.Clamp(
+                    right, _blinkSource.Right - openFromCloseDiff, _blinkSource.Right + openKeepDiff
+                );
 
-            _leftBlinkSpeed = Mathf.Lerp(_leftBlinkSpeed, leftSpeed, speedFactor * Time.deltaTime);
-            _rightBlinkSpeed = Mathf.Lerp(_rightBlinkSpeed, rightSpeed, speedFactor * Time.deltaTime);
-
-            _leftBlinkSpeed *= speedDumpFactor;
-            _rightBlinkSpeed *= speedDumpFactor;
-
-            _latestFilteredLeft += _leftBlinkSpeed * Time.deltaTime;
-            _latestFilteredRight += _rightBlinkSpeed * Time.deltaTime;
-
-            _blinkSource.Left = (_latestFilteredLeft > closeThreshold) ? 1.0f : _latestFilteredLeft;
-            _blinkSource.Right = (_latestFilteredRight > closeThreshold) ? 1.0f : _latestFilteredRight;
+                if (nextRight > _blinkSource.Right - openFromCloseDiff)
+                {
+                    _isRightEyeOpening = false;
+                }
+                
+                _blinkSource.Right = nextRight;
+            }
+            else
+            {
+                _blinkSource.Right = Mathf.Clamp(
+                    right, _blinkSource.Right - openKeepDiff, _blinkSource.Right + openKeepDiff
+                );
+            }
         }
     }
 }

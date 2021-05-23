1d369bbd9a5b3161b53a463f327420f20445147d
malaybaku <njslyr.trpg+harriet@gmail.com>
2021-05-23 16:56:00 +0900

    use serialization for mic/camera device names

---
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/InputMonitoring/DeviceData/DeviceNames.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Baku.VMagicMirror
+{
+    [Serializable]
+    public class DeviceNames
+    {
+        public string[] Names;
+
+        public static string CreateDeviceNamesJson(string[] names)
+        {
+            return JsonUtility.ToJson(new DeviceNames()
+            {
+                Names = names,
+            });
+        }
+    }
+}

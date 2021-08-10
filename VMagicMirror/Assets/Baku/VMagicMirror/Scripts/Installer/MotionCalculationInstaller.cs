421ff8537d2b7d02b2595a07bba21b99509c87d7
malaybaku <njslyr.trpg+harriet@gmail.com>
2021-08-11 00:16:22 +0900

    Add head nodding / shaking systems

--- VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Installer/MotionCalculationInstaller.cs
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Installer/MotionCalculationInstaller.cs
@@ -6,10 +6,12 @@ namespace Baku.VMagicMirror.Installer
     public class MotionCalculationInstaller : InstallerBase
     {
         [SerializeField] private FaceAttitudeController faceAttitude = null;
+        [SerializeField] private HeadMotionClipPlayer headMotionClipPlayer = null;
     
         public override void Install(DiContainer container)
         {
             container.BindInstance(faceAttitude).AsCached();
+            container.BindInstance(headMotionClipPlayer).AsCached();
         }
     }
 }

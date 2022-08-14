b31216068b756357bb91171baa1517d5bbaf32b3
malaybaku <njslyr.trpg+harriet@gmail.com>
2022-08-14 12:42:44 +0900

    Add "move eyes during face clip applied" option in UI

--- WPF/VMagicMirrorConfig/Model/InterProcess/Core/MessageFactory.cs
+++ WPF/VMagicMirrorConfig/Model/InterProcess/Core/MessageFactory.cs
@@ -145,6 +145,7 @@ namespace Baku.VMagicMirrorConfig
         public Message MicrophoneDeviceNames() => NoArg();
 
         public Message LookAtStyle(string v) => WithArg(v);
+        public Message EnableEyeMotionDuringClipApplied(bool enable) => WithArg(enable);
         public Message SetUseAvatarEyeBoneMap(bool use) => WithArg(use);
         public Message SetEyeBoneRotationScale(int percent) => WithArg(percent);
         public Message SetEyeBoneRotationScaleWithMap(int percent) => WithArg(percent);

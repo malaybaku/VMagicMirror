51cb42b91aeb469147d8a856aa0bd7932eb946ab
malaybaku <njslyr.trpg+harriet@gmail.com>
2022-09-08 00:33:55 +0900

    WIP: add presenter code

---
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/AvatarControl/WordToMotion/V2/WordToMotionPresenter.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UnityEngine;
+
+namespace Baku.VMagicMirror.WordToMotion
+{
+    public class WordToMotionPresenter : PresenterBase
+    {
+        private readonly IMessageReceiver _receiver;
+        private readonly WordToMotionRequestRepository _repository;
+        private readonly CustomMotionRepository _customMotionRepository;
+        private readonly WordToMotionRequester _requester;
+        private readonly IRequestSource[] _sources;
+
+        public WordToMotionPresenter(
+            IMessageReceiver receiver,
+            WordToMotionRequestRepository repository,
+            CustomMotionRepository customMotionRepository,
+            WordToMotionRequester requester,
+            IEnumerable<IRequestSource> sources
+            )
+        {
+            _receiver = receiver;
+            _repository = repository;
+            _customMotionRepository = customMotionRepository;
+            _requester = requester;
+            _sources = sources.ToArray();
+        }
+
+        public override void Initialize()
+        {
+            _receiver.AssignCommandHandler(
+                VmmCommands.ReloadMotionRequests,
+                message => ReloadMotionRequests(message.Content)
+            );
+            _receiver.AssignCommandHandler(
+                VmmCommands.PlayWordToMotionItem,
+                message => PlayWordToMotionItem(message.Content)
+            );
+            
+            _receiver.AssignCommandHandler(
+                VmmCommands.EnableWordToMotionPreview,
+                message => _requester.SetPreviewActive(message.ToBoolean())
+            );
+            
+            _receiver.AssignCommandHandler(
+                VmmCommands.SendWordToMotionPreviewInfo,
+                message => ReceiveWordToMotionPreviewInfo(message.Content)
+            );
+            
+            _receiver.AssignCommandHandler(
+                VmmCommands.SetDeviceTypeToStartWordToMotion,
+                message => SetWordToMotionInputType(message.ToInt())
+            );
+
+            //未実装なので一旦なしで
+            // _receiver.AssignCommandHandler(
+            //     VmmCommands.RequestCustomMotionDoctor,
+            //     _ => { }
+            // );
+            
+            _receiver.AssignQueryHandler(
+                VmmQueries.GetAvailableCustomMotionClipNames,
+                q =>
+                {
+                    q.Result = string.Join("\t", _customMotionRepository.LoadAvailableCustomMotionNames());
+                    Debug.Log("Get Available CustomMotion Clip Names, result = " + q.Result);
+                });
+
+            //リクエストは等価に扱う && 頻度は制御する、という話
+            _sources
+                .Select(source => source.RunMotionRequested.Select(index => (index, source.SourceType)))
+                .Merge()
+                .ThrottleFirst(TimeSpan.FromSeconds(0.3f))
+                .Subscribe(value => _requester.Play(value.index, value.SourceType))
+                .AddTo(this);
+        }
+        
+        private void SetWordToMotionInputType(int deviceType)
+        {
+            var type = (SourceType) deviceType;
+            _requester.SourceType = type;
+            foreach (var source in _sources)
+            {
+                source.SetActive(type == source.SourceType);
+            }
+        }
+
+        private void ReloadMotionRequests(string json)
+        {
+            try
+            {
+                _repository.Update(JsonUtility.FromJson<MotionRequestCollection>(json).Requests);
+            }
+            catch (Exception ex)
+            {
+                LogOutput.Instance.Write(ex);
+            }
+        }
+
+        private void PlayWordToMotionItem(string json)
+        {
+            try
+            {
+                _requester.Play(JsonUtility.FromJson<MotionRequest>(json));
+            }
+            catch (Exception ex)
+            {
+                LogOutput.Instance.Write(ex);
+            }
+        }
+
+        private void ReceiveWordToMotionPreviewInfo(string json)
+        {
+            try
+            {
+                _requester.SetPreviewRequest(JsonUtility.FromJson<MotionRequest>(json));
+            }
+            catch (Exception ex)
+            {
+                LogOutput.Instance.Write(ex);
+            }
+        }
+    }
+}

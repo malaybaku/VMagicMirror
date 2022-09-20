48baca213edf4f4b2847e146cf10ac891adc76e8
malaybaku <njslyr.trpg+harriet@gmail.com>
2022-09-20 21:55:03 +0900

    Remove always-false flag

--- VMagicMirror/Assets/Baku/VMagicMirror/Scripts/AvatarControl/FaceContol/FaceControlConfiguration.cs
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/AvatarControl/FaceContol/FaceControlConfiguration.cs
@@ -20,17 +20,7 @@ namespace Baku.VMagicMirror
         #endregion
         
         #region 内部的に特定クラスがsetterを呼ぶ値で、読み取り側は直接使わないでOK
-        
-        /// <summary>
-        /// Word to Motionによる表情指定が動いている状態かどうかを取得、設定します。
-        /// </summary>
-        /// <remarks>
-        /// setterを使っていいのは<see cref="WordToMotionManager"/>だけ。
-        /// これがtrueのとき、ほかのブレンドシェイプ関連のクラスではVRMBlendShapeProxyに対して
-        /// AccumulateとかApplyを呼ばないことが望ましいです。
-        /// </remarks>
-        public bool WordToMotionExpressionActive { get; set; }
-        
+
         /// <summary>
         /// 外部トラッカーによるFaceSwitchが動作しているかどうかを取得、設定します。
         /// </summary>
@@ -78,11 +68,7 @@ namespace Baku.VMagicMirror
         #endregion
         
         /// <summary> 口以外のブレンドシェイプ操作を一次的に適用停止すべきときtrueになります。 </summary>
-        public bool ShouldSkipNonMouthBlendShape => WordToMotionExpressionActive || FaceSwitchActive;
-
-        /// <summary> リップシンクを一次的に適用停止すべきときにtrueになります。 </summary>
-        public bool ShouldSkipMouthBlendShape => WordToMotionExpressionActive || FaceSwitchRequestStopLipSync;
-        
+        public bool ShouldSkipNonMouthBlendShape => FaceSwitchActive;
     }
 
     /// <summary>

185e06ccec971daf8a7844bce75b3f13a0fe043f
malaybaku <njslyr.trpg+harriet@gmail.com>
2023-01-21 14:55:50 +0900

    Add hand down ik calculator

--- VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Installer/MotionCalculationInstaller.cs
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Installer/MotionCalculationInstaller.cs
@@ -15,6 +15,7 @@ namespace Baku.VMagicMirror.Installer
 
         public override void Install(DiContainer container)
         {
+            container.Bind<HandDownIkCalculator>().AsSingle();
             container.BindInstances(
                 handIKIntegrator,
                 faceAttitude,

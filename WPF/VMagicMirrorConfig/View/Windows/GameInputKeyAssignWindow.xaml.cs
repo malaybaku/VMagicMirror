e1dbebd07158f1149f67d800516fd69cc3c924d5
Baku Dreameater <njslyr.trpg+harriet@gmail.com>
2023-03-21 22:54:21 +0900

    Add gamepad shaped UI / key assign view

---
+++ WPF/VMagicMirrorConfig/View/Windows/GameInputKeyAssignWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Baku.VMagicMirrorConfig.View
+{
+    //NOTE: 構造は詳細設定のウィンドウ = SettingWindowと同じで、1アプリに1ウィンドウまでの単独なウィンドウを想定している
+    public partial class GameInputKeyAssignWindow : MetroWindow
+    {
+        public GameInputKeyAssignWindow() => InitializeComponent();
+
+        /// <summary>現在設定ウィンドウがあればそれを取得し、なければnullを取得します。</summary>
+        public static GameInputKeyAssignWindow? CurrentWindow { get; private set; } = null;
+
+        public static void OpenOrActivateExistingWindow()
+        {
+            if (CurrentWindow == null)
+            {
+                CurrentWindow = new GameInputKeyAssignWindow()
+                {
+                    Owner = Application.Current.MainWindow,
+                    WindowStartupLocation = WindowStartupLocation.CenterOwner,
+                };
+
+                CurrentWindow.Closed += OnSettingWindowClosed;
+                CurrentWindow.Show();
+            }
+            else
+            {
+                CurrentWindow.Activate();
+            }
+        }
+
+        private static async void OnSettingWindowClosed(object? sender, EventArgs e)
+        {
+            if (CurrentWindow != null)
+            {
+                CurrentWindow.Closed -= OnSettingWindowClosed;
+                CurrentWindow = null;
+
+                //NOTE: ウィンドウを閉じたあとはGC可能なリソースがそこそこある(WindowとかViewModelとか)ので、明示的にやってしまう
+                await Task.Delay(1000);
+                GC.Collect();
+            }
+        }
+    }
+}

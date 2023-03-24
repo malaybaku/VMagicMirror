942b00518cb1929ce027b6b843e3e4f5af9bb692
Baku Dreameater <njslyr.trpg+harriet@gmail.com>
2023-03-24 23:20:38 +0900

    update game input impl

--- VMagicMirror/Assets/Baku/VMagicMirror/Scripts/AvatarControl/Motion/GamePadBasedBody/GamepadGameInputSource.cs
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/AvatarControl/Motion/GamePadBasedBody/GamepadGameInputSource.cs
@@ -1,62 +1,43 @@
 using System;
 using UniRx;
 using UnityEngine;
-using Zenject;
 
 namespace Baku.VMagicMirror.GameInput
 {
-    public enum GamepadMoveStickType
+    public class GamepadGameInputSource : PresenterBase, IGameInputSource
     {
-        None,
-        LeftStick,
-        RightStick,
-        LeftArrowButton,
-    }
+        //コントローラによっては0付近で値がカチカチするので…
+        private const float StickDeadZone = 0.15f;
 
-    //NOTE: めんどくさいからトリガーを一旦省いてるが、別に入れてもいい
-    public enum GamepadLocomotionButtonAssign
-    {
-        None,
-        A,
-        B,
-        X,
-        Y,
-    }
-
-    public class GamepadGameInputSource : PresenterBase, IGameInputSource, ITickable
-    {
-        private const float MoveInputDiffPerSecond = 2f;
-        private const float MoveInputDeadZone = 0.15f;
-
+        #region Interface
+        
         bool IGameInputSource.IsActive => _isActive;
         IObservable<Vector2> IGameInputSource.MoveInput => _moveInput;
+        IObservable<Vector2> IGameInputSource.LookAroundInput => _lookAroundInput;
         IObservable<bool> IGameInputSource.IsCrouching => _isCrouching;
-        public IObservable<Unit> Jump => _jump;
-
-        private readonly XInputGamePad _gamepad;
-
-        private bool _isActive;
-        private Vector2 _rawMoveInput;
-        private Vector2 _dumpedMoveInput;
+        IObservable<bool> IGameInputSource.IsRunning => _isRunning;
+        IObservable<Unit> IGameInputSource.Jump => _jump;
+        IObservable<Unit> IGameInputSource.Punch => _punch;
+        IObservable<Unit> IGameInputSource.GunTrigger => _gunTrigger;
         
-        private readonly ReactiveProperty<Vector2> _moveInput = new ReactiveProperty<Vector2>();
-        private readonly ReactiveProperty<bool> _isCrouching = new ReactiveProperty<bool>();
-        private readonly Subject<Unit> _jump = new Subject<Unit>();
-
-        //NOTE: スティックやジャンプ/しゃがみ指定の初期値はテキトーで、無いほうがマシなら未割り当てにしてもよい
-        private readonly ReactiveProperty<GamepadMoveStickType> _stickType =
-            new ReactiveProperty<GamepadMoveStickType>(GamepadMoveStickType.LeftStick);
-
-        public GamepadKey JumpButton { get; set; } = GamepadKey.A;
-        public GamepadKey CrouchButton { get; set; } = GamepadKey.X;
-
+        #endregion
+        
+        private readonly XInputGamePad _gamepad;
         private readonly IMessageReceiver _receiver;
         private CompositeDisposable _disposable;
 
-        //TODO: 設定も受け取らないといけない
-        public GamepadGameInputSource(
-            IMessageReceiver receiver,
-            XInputGamePad gamePad)
+        private readonly ReactiveProperty<Vector2> _moveInput = new ReactiveProperty<Vector2>();
+        private readonly ReactiveProperty<Vector2> _lookAroundInput = new ReactiveProperty<Vector2>();
+        private readonly ReactiveProperty<bool> _isCrouching = new ReactiveProperty<bool>();
+        private readonly ReactiveProperty<bool> _isRunning = new ReactiveProperty<bool>();
+        private readonly Subject<Unit> _jump = new Subject<Unit>();
+        private readonly Subject<Unit> _punch = new Subject<Unit>();
+        private readonly Subject<Unit> _gunTrigger = new Subject<Unit>();
+
+        private bool _isActive;
+        private GamepadGameInputKeyAssign _keyAssign = GamepadGameInputKeyAssign.LoadDefault();
+        
+        public GamepadGameInputSource(IMessageReceiver receiver, XInputGamePad gamePad)
         {
             _receiver = receiver;
             _gamepad = gamePad;
@@ -70,24 +51,42 @@ namespace Baku.VMagicMirror.GameInput
                 );
         }
 
-        void ITickable.Tick()
-        {
-            var diff = _rawMoveInput - _dumpedMoveInput;
-            _dumpedMoveInput += Vector2.ClampMagnitude(diff, Mathf.Min(
-                diff.magnitude,
-                MoveInputDiffPerSecond * Time.deltaTime
-            ));
-            _moveInput.Value = GetMoveInputWithDeadZone(_dumpedMoveInput);
-        }
-        
         private void ApplyGamepadKeyAssign(string content)
         {
-            
+            try
+            {
+                var setting = JsonUtility.FromJson<GamepadGameInputKeyAssign>(content);
+                ApplyKeyAssign(setting);
+            }
+            catch (Exception ex)
+            {
+                LogOutput.Instance.Write(ex);
+            }
+        }
+
+        private void ResetInput()
+        {
+            _moveInput.Value = Vector2.zero;
+            _lookAroundInput.Value = Vector2.zero;
+
+            _isRunning.Value = false;
+            _isCrouching.Value = false;
         }
         
+        private void ApplyKeyAssign(GamepadGameInputKeyAssign setting)
+        {
+            _keyAssign = setting;
+            if (_isActive)
+            {
+                SetActive(false);
+                SetActive(true);
+            }
+        }
+
         /// <summary>
         /// trueで呼び出すとゲームパッドの入力監視を開始する。
         /// falseで呼び出すと入力監視を終了する。必要ないうちは切っておくのを想定している
+        /// この処理はキーアサインが変わるときも呼ばれる
         /// </summary>
         /// <param name="active"></param>
         public void SetActive(bool active)
@@ -101,6 +100,7 @@ namespace Baku.VMagicMirror.GameInput
             _disposable?.Dispose();
             if (!active)
             {
+                ResetInput();
                 return;
             }
 
@@ -109,56 +109,127 @@ namespace Baku.VMagicMirror.GameInput
                 .Subscribe(OnButtonUpDown)
                 .AddTo(_disposable);
 
-            _stickType.Select(s => s switch
-                {
-                    GamepadMoveStickType.LeftStick => _gamepad.LeftStickPosition,
-                    GamepadMoveStickType.RightStick => _gamepad.RightStickPosition,
-                    GamepadMoveStickType.LeftArrowButton => _gamepad
-                        .ObserveEveryValueChanged(g => g.ArrowButtonsStickPosition),
-                    _ => Observable.Return(Vector2Int.zero),
-                })
-                .Switch()
-                .Subscribe(OnMoveInputChanged)
-                .AddTo(_disposable);
+            if (_keyAssign.StickLeft != GameInputStickAction.None)
+            {
+                _gamepad.LeftStickPosition
+                    .Subscribe(v => OnStickUpdated(v, _keyAssign.StickLeft))
+                    .AddTo(_disposable);
+            }
+            
+            if (_keyAssign.StickRight != GameInputStickAction.None)
+            {
+                _gamepad.RightStickPosition
+                    .Subscribe(v => OnStickUpdated(v, _keyAssign.StickLeft))
+                    .AddTo(_disposable);
+            }
+
+            if (_keyAssign.DPadLeft != GameInputStickAction.None)
+            {
+                _gamepad.ObserveEveryValueChanged(g => g.ArrowButtonsStickPosition)
+                    .Subscribe(v => OnStickUpdated(v, _keyAssign.StickLeft))
+                    .AddTo(_disposable);
+            }
         }
         
         private void OnButtonUpDown(GamepadKeyData data)
         {
-            if (data.Key == JumpButton && data.IsPressed)
+            var action = GetButtonAction(data.Key);
+            if (action == GameInputButtonAction.None)
             {
-                _jump.OnNext(Unit.Default);
+                return;
             }
 
-            if (data.Key == CrouchButton)
+            //NOTE: 複数ボタンに割当たっている入力は後勝ちさせる。ちょっと変だが安定はするので。
+            if (action == GameInputButtonAction.Run)
+            {
+                _isRunning.Value = data.IsPressed;
+                return;
+            }
+            
+            if (action == GameInputButtonAction.Crouch)
             {
                 _isCrouching.Value = data.IsPressed;
+                return;
+            }
+            
+            //Trigger系の挙動はボタン下げでのみ起こる
+            if (!data.IsPressed)
+            {
+                return;
+            }
+
+            switch (action)
+            {
+                case GameInputButtonAction.Punch:
+                    _punch.OnNext(Unit.Default);
+                    break;
+                case GameInputButtonAction.Trigger:
+                    _punch.OnNext(Unit.Default);
+                    break;
+                case GameInputButtonAction.Jump:
+                    _jump.OnNext(Unit.Default);
+                    break;
             }
         }
 
-        private void OnMoveInputChanged(Vector2Int moveInput)
+        private void OnStickUpdated(Vector2Int value, GameInputStickAction action)
         {
-            Debug.Log("OnMoveINputChanged");
-            var input = new Vector2(moveInput.x * 1f / 32768f, moveInput.y * 1f / 32768f);
-
-            //NOTE: 更に「カメラに向かって奥方向かどうか」みたいなフラグも配慮したい気がする
-            _rawMoveInput = input;
+            if (action == GameInputStickAction.None)
+            {
+                return;
+            }
+            
+            var input = new Vector2(value.x * 1f / 32768f, value.y * 1f / 32768f);
+            switch (action)
+            {
+                case GameInputStickAction.Move:
+                    _moveInput.Value = GetInputWithDeadZone(input);
+                    return;
+                case GameInputStickAction.LookAround:
+                    _lookAroundInput.Value = GetInputWithDeadZone(input);
+                    return;
+            }
         }
 
-        
-        
-        private static Vector2 GetMoveInputWithDeadZone(Vector2 input)
+        private static Vector2 GetInputWithDeadZone(Vector2 input)
         {
             var magnitude = input.magnitude;
             // - 0 ~ DeadZone: ゼロ
             // - DeadZone ~ 1: 方向そのまま、大きさ0~1のベクトルに変形して割当
-            if (magnitude < MoveInputDeadZone)
+            if (magnitude < StickDeadZone)
             {
                 return Vector2.zero;
             }
             else
             {
-                return input / magnitude * Mathf.InverseLerp(MoveInputDeadZone, 1f, magnitude);
+                return input / magnitude * Mathf.InverseLerp(StickDeadZone, 1f, magnitude);
             }
+        }
+
+        private GameInputButtonAction GetButtonAction(GamepadKey key)
+        {
+            return key switch
+            {
+                GamepadKey.A => _keyAssign.ButtonA,
+                GamepadKey.B => _keyAssign.ButtonB,
+                GamepadKey.X => _keyAssign.ButtonX,
+                GamepadKey.Y => _keyAssign.ButtonY,
+                GamepadKey.LShoulder => _keyAssign.ButtonLButton,
+                GamepadKey.LTrigger => _keyAssign.ButtonLTrigger,
+                GamepadKey.RShoulder => _keyAssign.ButtonRButton,
+                GamepadKey.RTrigger=> _keyAssign.ButtonRTrigger,
+                GamepadKey.Select => _keyAssign.ButtonView,
+                GamepadKey.Start => _keyAssign.ButtonMenu,
+                
+                _ => GameInputButtonAction.None,
+            };
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            _disposable?.Dispose();
+            _disposable = null;
         }
     }
 }
--- VMagicMirror/Assets/Baku/VMagicMirror/Scripts/AvatarControl/Motion/GamePadBasedBody/KeyboardGameInputSource.cs
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/AvatarControl/Motion/GamePadBasedBody/KeyboardGameInputSource.cs
@@ -1,77 +1,115 @@
 using System;
+using System.Windows.Forms;
 using UniRx;
 using UnityEngine;
+using Zenject;
 
 namespace Baku.VMagicMirror.GameInput
 {
-    public enum KeyboardMoveInputStyles
+    public class KeyboardGameInputSource : PresenterBase, ITickable, IGameInputSource
     {
-        None,
-        Wasd,
-        ArrowKey,
-    }
-    
-    public class KeyboardGameInputSource : IGameInputSource, IDisposable
-    {
+        //NOTE: DPIが96の場合の値
+        private const float LookAroundNormalizeFactor = 100;
+        private const float MouseMoveThrottleCount = 0.4f;
+
+        #region Interface 
+        
         bool IGameInputSource.IsActive => _isActive;
         IObservable<Vector2> IGameInputSource.MoveInput => _moveInput;
-        IObservable<Unit> IGameInputSource.Jump => _jump;
-        IObservable<bool> IGameInputSource.IsCrouching => _isCrouching;
+        IObservable<Vector2> IGameInputSource.LookAroundInput => _lookAroundInput;
 
+        IObservable<bool> IGameInputSource.IsCrouching => _isCrouching;
+        IObservable<bool> IGameInputSource.IsRunning => _isRunning;
+        IObservable<Unit> IGameInputSource.Jump => _jump;
+        IObservable<Unit> IGameInputSource.Punch => _punch;
+        IObservable<Unit> IGameInputSource.GunTrigger => _gunTrigger;
+
+        #endregion
+        
         private bool _isActive;
         private readonly ReactiveProperty<Vector2> _moveInput = new ReactiveProperty<Vector2>();
+        private readonly ReactiveProperty<Vector2> _lookAroundInput = new ReactiveProperty<Vector2>();
         private readonly ReactiveProperty<bool> _isCrouching = new ReactiveProperty<bool>();
+        private readonly ReactiveProperty<bool> _isRunning = new ReactiveProperty<bool>();
         private readonly Subject<Unit> _jump = new Subject<Unit>();
+        private readonly Subject<Unit> _punch = new Subject<Unit>();
+        private readonly Subject<Unit> _gunTrigger = new Subject<Unit>();
+
+        private readonly IKeyMouseEventSource _keySource;
+        private readonly IMessageReceiver _receiver;
+        private readonly MousePositionProvider _mousePositionProvider;
 
         private CompositeDisposable _disposable;
-        private readonly IKeyMouseEventSource _keySource;
-
-        //NOTE: KeyCodeにしたいよねえ…
-        public string JumpKeyName { get; set; } = "Space";
-        public string CrouchKeyName { get; set; } = "C";
-
-        private bool _useMoveKey;
-        private string _moveForwardKey = "W";
-        private string _moveBackKey = "S";
-        private string _moveLeftKey = "A";
-        private string _moveRightKey = "D";
-
+        private KeyboardGameInputKeyAssign _keyAssign;
+        
         private bool _forwardKeyPressed;
         private bool _backKeyPressed;
         private bool _leftKeyPressed;
         private bool _rightKeyPressed;
+
+        //マウスが動き続けている間はその移動値が蓄積され、そうでなければ0になるような値
+        private Vector2Int _rawDiffSum;
+        //マウスが動き続けている間は0より大きくなる値
+        private float _mouseMoveCountDown;
         
-        //TODO: いろいろな依存関係(あれば)
-        public KeyboardGameInputSource(IKeyMouseEventSource keySource)
+        public KeyboardGameInputSource(
+            IKeyMouseEventSource keySource,
+            MousePositionProvider mousePositionProvider,
+            IMessageReceiver receiver)
         {
             _keySource = keySource;
+            _receiver = receiver;
+            _mousePositionProvider = mousePositionProvider;
         }
 
-        public void SetMoveInputStyle(KeyboardMoveInputStyles style)
+        public override void Initialize()
         {
-            switch (style)
+            _receiver.AssignCommandHandler(
+                VmmCommands.SetKeyboardGameInputKeyAssign,
+                command => UpdateKeyAssign(command.Content)
+                );
+        }
+
+        void ITickable.Tick()
+        {
+            if (!_isActive)
             {
-                case KeyboardMoveInputStyles.None:
-                    _useMoveKey = false;
-                    _moveInput.Value = Vector2.zero;
-                    return;
-                case KeyboardMoveInputStyles.Wasd:
-                    _useMoveKey = true;
-                    _moveForwardKey = "W";
-                    _moveBackKey = "S";
-                    _moveLeftKey = "A";
-                    _moveRightKey = "D";
-                    break;
-                case KeyboardMoveInputStyles.ArrowKey:
-                    _useMoveKey = true;
-                    _moveForwardKey = "Up";
-                    _moveBackKey = "Down";
-                    _moveLeftKey = "Left";
-                    _moveRightKey = "Right";
-                    break;
+                return;
+            }
+
+            //マウス移動の扱い
+            // - マウスの位置ではなく移動値に頼る。FPSだとマウスロックかかる事もあるのでそれ前提で考える
+            // - ちょっとだけmagnitudeも使うが、基本的に首がガン振りされてよい前提で考える
+            // - 一定時間動かないのを以て「入力がゼロに戻った」とみなす
+            var rawDiff = _mousePositionProvider.RawDiff;
+            if (rawDiff == Vector2Int.zero)
+            {
+                if (_mouseMoveCountDown > 0f)
+                {
+                    _mouseMoveCountDown -= Time.deltaTime;
+                    if (_mouseMoveCountDown <= 0f)
+                    {
+                        _rawDiffSum = Vector2Int.zero;
+                        _lookAroundInput.Value = Vector2.zero;
+                    }
+                }
+            }
+            else
+            {
+                //NOTE: この積算だと「右上 -> 左上」みたくマウス動かしたときに真上を指すが、それは想定挙動とする
+                _rawDiffSum += rawDiff;
+                var dpiRate = Screen.dpi / 96f;
+                if (dpiRate <= 0f)
+                {
+                    dpiRate = 1f;
+                }
+
+                var input = (dpiRate / LookAroundNormalizeFactor) * new Vector2(_rawDiffSum.x, _rawDiffSum.y);
+                _lookAroundInput.Value = Vector2.ClampMagnitude(input, 1f);
+                _mouseMoveCountDown = MouseMoveThrottleCount;
             }
         }
-
+        
         public void SetActive(bool active)
         {
             if (_isActive == active)
@@ -83,6 +121,7 @@ namespace Baku.VMagicMirror.GameInput
             _disposable?.Dispose();
             if (!active)
             {
+                ResetInputs();
                 return;
             }
 
@@ -93,73 +132,195 @@ namespace Baku.VMagicMirror.GameInput
             _keySource.RawKeyDown
                 .Subscribe(OnKeyDown)
                 .AddTo(_disposable);
+
+            _keySource.MouseButton
+                .Subscribe(OnMouseKeyUpDown)
+                .AddTo(_disposable);
+        }
+
+        private void UpdateKeyAssign(string json)
+        {
+            try
+            {
+                var setting = JsonUtility.FromJson<KeyboardGameInputKeyAssign>(json);
+                ApplyKeyAssign(setting);
+            }
+            catch (Exception ex)
+            {
+                LogOutput.Instance.Write(ex);
+            }
         }
         
+        private void ApplyKeyAssign(KeyboardGameInputKeyAssign setting)
+        {
+            _keyAssign = setting;
+            if (_isActive)
+            {
+                SetActive(false);
+                SetActive(true);
+            }
+        }
+
+        private void ResetInputs()
+        {
+            _rawDiffSum = Vector2Int.zero;
+            _mouseMoveCountDown = 0f;
+
+            _moveInput.Value = Vector2.zero;
+            _lookAroundInput.Value = Vector2.zero;
+            _isRunning.Value = false;
+            _isCrouching.Value = false;
+
+            _forwardKeyPressed = false;
+            _backKeyPressed = false;
+            _leftKeyPressed = false;
+            _rightKeyPressed = false;
+        }
+
         private void OnKeyDown(string key)
         {
-            //TODO?: 押すより離した瞬間とかのほうが良い？どっちにしても微妙か？
-            if (key == JumpKeyName)
+            if (_keyAssign.UseShiftRun && 
+                (key == nameof(Keys.ShiftKey) || key == nameof(Keys.LShiftKey) || key == nameof(Keys.RShiftKey))
+                )
             {
-                _jump.OnNext(Unit.Default);
+                _isRunning.Value = true;
+                //shiftはコレ以外の入力に使ってないはずなので無視
+                return;
             }
 
-            if (key == CrouchKeyName)
+            if (_keyAssign.UseSpaceJump && key == nameof(Keys.Space))
             {
-                _isCrouching.Value = true;
+                //Spaceもこの用途でのみ使うはず
+                _jump.OnNext(Unit.Default);
+                return;
             }
-            
-            if (_useMoveKey)
+
+            if (_keyAssign.UseWasdMove)
             {
-                UpdateMoveInput(key, true);
+                _forwardKeyPressed = _forwardKeyPressed || key == "W";
+                _leftKeyPressed = _leftKeyPressed || key == "A";
+                _backKeyPressed = _backKeyPressed || key == "S";
+                _rightKeyPressed = _rightKeyPressed || key == "D";
+                return;
             }
-            else
+
+            if (_keyAssign.UseArrowKeyMove)
             {
-                _moveInput.Value = Vector2.zero;
+                _forwardKeyPressed = _forwardKeyPressed || key == nameof(Keys.Up);
+                _leftKeyPressed = _leftKeyPressed || key == nameof(Keys.Left);
+                _backKeyPressed = _backKeyPressed || key == nameof(Keys.Down);
+                _rightKeyPressed = _rightKeyPressed || key == nameof(Keys.Right);
+                return;
             }
+
+            //NOTE: MoveInputは変化しないことのほうが多いが、冗長に呼んでおく
+            UpdateMoveInput();
         }
 
         private void OnKeyUp(string key)
         {
-            if (key == CrouchKeyName)
+            if (_keyAssign.UseShiftRun && 
+                (key == nameof(Keys.ShiftKey) || key == nameof(Keys.LShiftKey) || key == nameof(Keys.RShiftKey))
+               )
             {
-                _isCrouching.Value = false;
+                _isRunning.Value = false;
+                return;
             }
-            
-            if (_useMoveKey)
+
+            if (_keyAssign.UseWasdMove)
             {
-                UpdateMoveInput(key, false);
+                if (key == "W") _forwardKeyPressed = false;
+                if (key == "A") _leftKeyPressed = false;
+                if (key == "S") _backKeyPressed = false;
+                if (key == "D") _rightKeyPressed = false;
             }
-            else
+
+            if (_keyAssign.UseArrowKeyMove)
             {
-                _moveInput.Value = Vector2.zero;
+                if (key == nameof(Keys.Up)) _forwardKeyPressed = false;
+                if (key == nameof(Keys.Left)) _leftKeyPressed = false;
+                if (key == nameof(Keys.Down)) _backKeyPressed = false;
+                if (key == nameof(Keys.Right)) _rightKeyPressed = false;
+            }
+
+            //NOTE: MoveInputは変化しないことのほうが多いが、まあそれはそれとして。
+            UpdateMoveInput();
+        }
+
+        private void OnMouseKeyUpDown(string button)
+        {
+            switch (button)
+            {
+                case MouseButtonEventNames.LDown:
+                    ApplyMouseInput(_keyAssign.LeftClick, true);
+                    return;
+                case MouseButtonEventNames.LUp:
+                    ApplyMouseInput(_keyAssign.LeftClick, false);
+                    return;
+                case MouseButtonEventNames.RDown:
+                    ApplyMouseInput(_keyAssign.RightClick, true);
+                    return;
+                case MouseButtonEventNames.RUp:
+                    ApplyMouseInput(_keyAssign.RightClick, false);
+                    return;
+                case MouseButtonEventNames.MDown:
+                    ApplyMouseInput(_keyAssign.MiddleClick, true);
+                    return;
+                case MouseButtonEventNames.MUp:
+                    ApplyMouseInput(_keyAssign.MiddleClick, false);
+                    return;
             }
         }
 
-        private void UpdateMoveInput(string key, bool isDown)
+        private void ApplyMouseInput(GameInputButtonAction action, bool pressed)
         {
-            if (key == _moveForwardKey)
+            if (action == GameInputButtonAction.None)
             {
-                _forwardKeyPressed = isDown;
-            }
-            else if (key == _moveBackKey)
-            {
-                _backKeyPressed = isDown;
-            }
-            else if (key == _moveLeftKey)
-            {
-                _leftKeyPressed = isDown;
-            }
-            else if (key == _moveRightKey)
-            {
-                _rightKeyPressed = isDown;
+                return;
             }
 
+            switch (action)
+            {
+                case GameInputButtonAction.Run:
+                    _isRunning.Value = pressed;
+                    return;
+                case GameInputButtonAction.Crouch:
+                    _isCrouching.Value = pressed;
+                    return;
+            }
+
+            if (!pressed)
+            {
+                return;
+            }
+
+            switch (action)
+            {
+                case GameInputButtonAction.Jump:
+                    _jump.OnNext(Unit.Default);
+                    return;
+                case GameInputButtonAction.Punch:
+                    _punch.OnNext(Unit.Default);
+                    return;
+                case GameInputButtonAction.Trigger:
+                    _gunTrigger.OnNext(Unit.Default);
+                    return;
+            }
+        }
+        
+        private void UpdateMoveInput()
+        {
             _moveInput.Value = new Vector2(
                 (_rightKeyPressed ? 1f : 0f) + (_leftKeyPressed ? -1f : 0f),
                 (_forwardKeyPressed ? 1f : 0f) + (_backKeyPressed ? -1f : 0f)
-                );
+            );
         }
 
-        void IDisposable.Dispose() => _disposable?.Dispose();
+        public override void Dispose()
+        {
+            base.Dispose();
+            _disposable?.Dispose();
+            _disposable = null;
+        }
     }
 }

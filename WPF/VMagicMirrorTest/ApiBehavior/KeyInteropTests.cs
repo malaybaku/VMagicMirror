31f62262547acca7671a9fe0a469695db2397b2c
Baku Dreameater <njslyr.trpg+harriet@gmail.com>
2023-05-16 00:52:17 +0900

    Add learning test to confirm Shift/Ctrl/Alt/Win key will be mapped to WinForms key code correctly

---
+++ WPF/VMagicMirrorTest/ApiBehavior/KeyInteropTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.Windows.Input;
+
+namespace Baku.VMagicMirror.Test.ApiBehavior
+{
+    public class KeyInteropTests
+    {
+        [Test]
+        public void VirtualKeyFromKey_アルファベットは普通に通る()
+        {
+            //NOTE: 65 = Windows.Forms.Keys.A
+            const int Start = 65;
+            for (var i = 0; i < 26; i++)
+            {
+                var k = KeyInterop.VirtualKeyFromKey(Key.A + i);
+                Assert.AreEqual(k, Start + i);
+            }
+        }
+
+        [TestCase(Key.LWin, 91)]
+        [TestCase(Key.RWin, 92)]
+        [TestCase(Key.LeftShift, 160)]
+        [TestCase(Key.RightShift, 161)]
+        [TestCase(Key.LeftCtrl, 162)]
+        [TestCase(Key.RightCtrl, 163)]
+        [TestCase(Key.LeftAlt, 164)]
+        [TestCase(Key.RightAlt, 165)]
+        [Test]
+        public void VirtualKeyFromKey_特殊なキーが想定通りにマップされる(Key key, int num)
+        {
+            var k = KeyInterop.VirtualKeyFromKey(key);
+            Assert.AreEqual(num, k);
+        }
+    }
+}

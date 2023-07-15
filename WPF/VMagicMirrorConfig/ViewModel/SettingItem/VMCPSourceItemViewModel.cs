97a0ede9f6360547f16729b00aa73f5807b52c96
Baku Dreameater <njslyr.trpg+harriet@gmail.com>
2023-07-15 17:49:12 +0900

    Add VMCP setting UI

---
+++ WPF/VMagicMirrorConfig/ViewModel/SettingItem/VMCPSourceItemViewModel.cs
@@ -0,0 +1,44 @@
+﻿namespace Baku.VMagicMirrorConfig.ViewModel
+{
+    //NOTE: コマンドベースでApplyしないと適用されんという使い方にする (これはポート番号の編集が絡むため)。
+    //そのため、RPropertyは基本使わず、
+    public class VMCPSourceItemViewModel : ViewModelBase
+    {
+        //TODO: モデルが持ってる初期値を受け取りたい
+        public VMCPSourceItemViewModel()
+        {
+            PortNumberIsInvalid = new RProperty<bool>(false);
+            Port = new RProperty<string>("", v =>
+            {
+                //フォーマット違反になってないかチェック。空欄の場合、ポートの利用予定が無いものと見て正常系扱いする
+                PortNumberIsInvalid.Value = 
+                    !string.IsNullOrEmpty(v) &&
+                    !(int.TryParse(v, out int i) && i >= 0 && i < 65536);
+            });
+
+            ResetCommand = new ActionCommand(ResetContent);
+        }
+
+        public string Name { get; set; } = "";
+
+        //NOTE: 実際は0-65535の間の数字であってほしい
+        public RProperty<string> Port { get; }
+
+        public RProperty<bool> PortNumberIsInvalid { get; }
+
+        public bool ReceiveHeadPose { get; set; }
+        public bool ReceiveFacial { get; set; }
+        public bool ReceiveHandPose { get; set; }
+
+        public ActionCommand ResetCommand { get; }
+
+        public void ResetContent()
+        {
+            Name = "";
+            Port.Value = "0";
+            ReceiveHeadPose = false;
+            ReceiveFacial = false;
+            ReceiveHandPose = false;
+        }
+    }
+}
---
+++ WPF/VMagicMirrorConfig/ViewModel/SettingWindowTab/VMCPSettingViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baku.VMagicMirrorConfig.ViewModel
+{
+    public class VMCPSettingViewModel : SettingViewModelBase
+    {
+        public VMCPSettingViewModel() 
+        {
+            _isDirty = new RProperty<bool>(false, _ => UpdateCanApply());
+        }
+
+        public VMCPSourceItemViewModel Source1 { get; private set; }
+        public VMCPSourceItemViewModel Source2 { get; private set; }
+        public VMCPSourceItemViewModel Source3 { get; private set; }
+
+        public RProperty<bool> CanApply = new (false);
+
+        private readonly RProperty<bool> _isDirty;
+
+        public void SetDirty() => _isDirty.Value = true;
+        
+        public ActionCommand ApplyChangeCommand { get; }
+        public ActionCommand RevertChangeCommand { get; }
+
+        private void UpdateCanApply()
+        {
+            CanApply.Value = _isDirty.Value &&
+                !Source1.PortNumberIsInvalid.Value &&
+                !Source2.PortNumberIsInvalid.Value &&
+                !Source3.PortNumberIsInvalid.Value;
+        }
+
+        private void LoadCurrentSettings()
+        {
+            //TODO: Modelから何か持ってくる
+            _isDirty.Value = false;
+        }
+
+        private void SaveCurrentSettings()
+        {
+            //TODO: Modelに何かぶつける
+            _isDirty.Value = false;
+        }
+    }
+}

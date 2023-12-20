dcd9ffadd7ed704c8051706ac58025e7c5dd9c1d
Baku Dreameater <njslyr.trpg+harriet@gmail.com>
2023-12-20 22:47:22 +0900

    treat custom motion list initialization status

--- WPF/VMagicMirrorConfig/Model/InterProcess/Util/CustomMotionList.cs
+++ WPF/VMagicMirrorConfig/Model/InterProcess/Util/CustomMotionList.cs
@@ -17,11 +17,26 @@ namespace Baku.VMagicMirrorConfig
 
         private readonly IMessageSender _sender;
 
-        private readonly ObservableCollection<string> _customMotionClipNames = new ObservableCollection<string>();
+        private readonly ObservableCollection<string> _customMotionClipNames = new();
         public ReadOnlyObservableCollection<string> CustomMotionClipNames { get; }
 
-        private readonly ObservableCollection<string> _vrmaCustomMotionClipNames = new ObservableCollection<string>();
+        private readonly ObservableCollection<string> _vrmaCustomMotionClipNames = new();
         public ReadOnlyObservableCollection<string> VrmaCustomMotionClipNames { get; }
+
+        private readonly TaskCompletionSource<bool> _customMotionsInitialized = new();
+
+        public async Task WaitCustomMotionsCompletedAsync()
+        {
+            await _customMotionsInitialized.Task;
+        }
+
+        private readonly object _isInitializedLock = new();
+        private bool _isInitialized = false;
+        public bool IsInitialized
+        {
+            get { lock (_isInitializedLock) return _isInitialized; }
+            private set { lock (_isInitializedLock) _isInitialized = value; }
+        }
 
         public async Task InitializeCustomMotionClipNamesAsync()
         {
@@ -37,6 +52,10 @@ namespace Baku.VMagicMirrorConfig
             {
                 _vrmaCustomMotionClipNames.Add(name);
             }
+
+            IsInitialized = true;
+            _customMotionsInitialized.SetResult(true);
+
         }
 
         private async Task<string[]> GetCustomMotionClipNamesAsync(bool vrmaOnly)

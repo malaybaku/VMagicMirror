76e1a57d3f30849e9d3ab398fe6f463ef8e26ffa
Baku Dreameater <njslyr.trpg+harriet@gmail.com>
2024-10-27 11:54:22 +0900

    main fix: hide gamepad when avatar's hand is not on gamepad

--- VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Environment/DeviceTransforms/GamepadVisibilityReceiver.cs
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Environment/DeviceTransforms/GamepadVisibilityReceiver.cs
@@ -1,5 +1,7 @@
-﻿using Deform;
+﻿using System;
+using Deform;
 using DG.Tweening;
+using UniRx;
 using UnityEngine;
 using Zenject;
 
@@ -10,51 +12,82 @@ namespace Baku.VMagicMirror
     {
         //TODO: 非MonoBehaviour化できそう
         [Inject]
-        public void Initialize(IMessageReceiver receiver, DeformableCounter deformableCounter)
+        public void Initialize(
+            DeviceVisibilityManager deviceVisibilityManager,
+            BodyMotionModeController bodyMotionModeController,
+            HandIKIntegrator handIKIntegrator,
+            DeformableCounter deformableCounter)
         {
+            _deviceVisibilityManager = deviceVisibilityManager;
+            _bodyMotionModeController = bodyMotionModeController;
+            _handIkIntegrator = handIKIntegrator;
             _deformableCounter = deformableCounter;
-            receiver.AssignCommandHandler(
-                VmmCommands.GamepadVisibility,
-                message =>
-                {
-                    _gamepadDeviceVisible = message.ToBoolean();
-                    SetGamepadVisibility();
-                });
-            receiver.AssignCommandHandler(
-                VmmCommands.SetGamepadMotionMode,
-                v =>
-                {
-                    _gamepadMotionMode = (GamepadMotionModes) v.ToInt();
-                    SetGamepadVisibility();
-                });
         }
 
-        private bool _gamepadDeviceVisible;
-        private GamepadMotionModes _gamepadMotionMode = GamepadMotionModes.Gamepad;
-
-
+        private DeviceVisibilityManager _deviceVisibilityManager;
+        private BodyMotionModeController _bodyMotionModeController;
+        private HandIKIntegrator _handIkIntegrator;
         private DeformableCounter _deformableCounter;
+
         private MagnetDeformer _deformer = null;
-        private Renderer[] _renderers = new Renderer[0];
-        private bool _latestVisibility = false;
-        
-        public bool IsVisible => _latestVisibility;
+        private Renderer[] _renderers = Array.Empty<Renderer>();
+
+        public bool IsVisible { get; private set; }
+
+        // trueの場合、ゲームパッドから両手が離れるとゲームパッドが非表示になる。
+        // 実質const値だが、設定で変えうるのでRP<bool>にしてある
+        private readonly ReactiveProperty<bool> _hideWhenHandIsNotOnGamepad = new(true);
 
         private void Start()
         {
             _deformer = GetComponent<MagnetDeformer>();
             _renderers = GetComponentsInChildren<Renderer>();
+
+            //NOTE: 初期値で1回だけ発火してほしいので最初だけAsUnitObservableになっている
+            Observable.Merge(
+                _deviceVisibilityManager.GamepadVisible.AsUnitObservable(),
+                _hideWhenHandIsNotOnGamepad.AsUnitWithoutLatest(),
+                _bodyMotionModeController.MotionMode.AsUnitWithoutLatest(),
+                _bodyMotionModeController.GamepadMotionMode.AsUnitWithoutLatest(),
+                _handIkIntegrator.LeftTargetType.AsUnitWithoutLatest(),
+                _handIkIntegrator.RightTargetType.AsUnitWithoutLatest()
+                )
+                .Subscribe(_ => SetGamepadVisibility(IsGamepadVisible()))
+                .AddTo(this);
         }
 
-        private void SetGamepadVisibility()
+        private bool IsGamepadVisible()
         {
-            bool visible = _gamepadDeviceVisible && _gamepadMotionMode == GamepadMotionModes.Gamepad;
-            if (visible == _latestVisibility)
+            // 設定の組み合わせに基づいたvisibilityをチェック
+            var settingBasedResult = 
+                _deviceVisibilityManager.GamepadVisible.Value &&
+                _bodyMotionModeController.MotionMode.Value is BodyMotionMode.Default &&
+                _bodyMotionModeController.GamepadMotionMode.Value is GamepadMotionModes.Gamepad;
+
+            if (!settingBasedResult)
+            {
+                return false;
+            }
+
+            if (!_hideWhenHandIsNotOnGamepad.Value)
+            {
+                return true;
+            }
+
+            // この行まで到達した場合、設定に加えて手IKの状態も検証される
+            return
+                _handIkIntegrator.LeftTargetType.Value is HandTargetType.Gamepad ||
+                _handIkIntegrator.RightTargetType.Value is HandTargetType.Gamepad;
+        }
+        
+        private void SetGamepadVisibility(bool visible)
+        {
+            if (visible == IsVisible)
             {
                 return;
             }
             
-            _latestVisibility = visible;
+            IsVisible = visible;
             DOTween
                 .To(
                     () => _deformer.Factor, 
@@ -78,7 +111,7 @@ namespace Baku.VMagicMirror
                     _deformableCounter.Decrement();
                     foreach (var r in _renderers)
                     {
-                        r.enabled = _latestVisibility;
+                        r.enabled = IsVisible;
                     }
                 });
         }

714115c289888267c735713235361c9b4d83b24f
Baku Dreameater <njslyr.trpg+harriet@gmail.com>
2025-04-25 15:07:37 +0900

    Update message write/read to reduce wait time for r/w ready

--- VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Interprocess/MemoryMappedFile/MemoryMappedFileConnectReader.cs
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Interprocess/MemoryMappedFile/MemoryMappedFileConnectReader.cs
@@ -9,6 +9,10 @@ namespace Baku.VMagicMirror.Mmf
 {
     public partial class MemoryMappedFileConnector
     {
+        // 基本的なreaderのデータ読み込みの流れ:
+        // - MMFの冒頭から順にデータを読み込んでいく
+        //   - チャンクの先頭1バイトが `0` 以外になるまで待つ → チャンクを読む → 読み込みindexを加算…を繰り返す
+        // - 特に、チャンクの先頭バイトが `2` のチャンクを検出したら、indexを0に戻しつつ、MMFの冒頭1バイトを `0` にリセット
         public sealed class InternalReader
         {
             private readonly object _readLock = new();
@@ -16,17 +20,11 @@ namespace Baku.VMagicMirror.Mmf
             private MemoryMappedFile? _file;
             private MemoryMappedViewAccessor? _accessor;
 
-            private bool CanReadMessage
-            {
-                get
-                {
-                    lock (_readLock) return _accessor?.ReadByte(0) == MessageStateReadReady;
-                }
-            }
+            private int _index = 0;
 
             public event Action<ReadOnlyMemory<byte>>? ReceiveCommand;
-            public event Action<(int id, ReadOnlyMemory<byte> content)>? ReceiveQuery;
-            public event Action<(int id, ReadOnlyMemory<byte> content)>? ReceiveQueryResponse;
+            public event Action<(ushort queryId, ReadOnlyMemory<byte> content)>? ReceiveQuery;
+            public event Action<(ushort queryId, ReadOnlyMemory<byte> content)>? ReceiveQueryResponse;
             
             
             public async Task RunAsync(MemoryMappedFile file, CancellationToken token)
@@ -66,7 +64,7 @@ namespace Baku.VMagicMirror.Mmf
                 await WaitReadReadyAsync(token);
 
                 var isReply = false;
-                var id = 0;
+                ushort queryId = 0;
                 var totalDataLength = 0;
 
                 // メッセージのBody以外の情報はここで確定
@@ -76,11 +74,11 @@ namespace Baku.VMagicMirror.Mmf
                     {
                         return;
                     }
-                    
-                    var messageType = _accessor.ReadInt16(2);
-                    isReply = messageType > 0;
-                    id = _accessor.ReadInt32(4);
-                    totalDataLength = _accessor.ReadInt32(8);
+
+                    var accessorOffset = _index;
+                    isReply = _accessor.ReadByte(accessorOffset + 1) == MessageTypeResponse;
+                    queryId = _accessor.ReadUInt16(accessorOffset + 2);
+                    totalDataLength = _accessor.ReadInt32(accessorOffset + 4);
                 }
 
                 // 使いまわし用のBufferより大きいデータが来そうな場合だけ別でallocする。このallocは滅多に起きない想定
@@ -102,10 +100,17 @@ namespace Baku.VMagicMirror.Mmf
                         {
                             return;
                         }
+
+                        var accessorOffset = _index;
                         
-                        dataLength = _accessor.ReadInt32(12);
-                        _accessor.ReadArray(16, messageBytes, offset, dataLength);
-                        _accessor.Write(0, (byte)0);
+                        // NOTE:
+                        // - チャンクの1~7byteは分割されたメッセージの2つ目以降でも同じ値が入っているので、コレ以降のループでは再読み込みしない
+                        // - dataLengthがMMFの末尾からはみ出さないことはwriterが保証してるはずなので、わざわざ検証しない
+                        dataLength = _accessor.ReadInt32(accessorOffset + 8);
+                        _accessor.ReadArray(accessorOffset + 12, messageBytes, offset, dataLength);
+                        // _accessor.Write(0, (byte)0);
+
+                        _index += dataLength + MessageHeaderSize;
                     }
 
                     offset += dataLength;
@@ -123,18 +128,53 @@ namespace Baku.VMagicMirror.Mmf
                 // messageの読み込みより前に次のデータ読み込みを行ってbufferが上書きされうるので、別で用意したほうが安全である
                 var message = new byte[totalDataLength];
                 Array.Copy(messageBytes, message, totalDataLength);
-                HandleReceivedMessage(message, id, isReply);
+                HandleReceivedMessage(message, queryId, isReply);
             }
 
             private async Task WaitReadReadyAsync(CancellationToken token)
             {
-                while (!CanReadMessage)
+                while (!token.IsCancellationRequested)
                 {
-                    await Task.Delay(1, token);
+                    byte state = 0;
+                    lock (_readLock)
+                    {
+                        if (_accessor == null)
+                        {
+                            return;
+                        }
+                        
+                        state = _accessor.ReadByte(_index);
+                    }
+
+                    switch (state)
+                    {
+                        case MessageStateEmpty:
+                            // 何もないので待つ: アプリ起動中の大体の時間はここを通過し続ける
+                            await Task.Delay(1, token);
+                            break;
+                        case MessageStateDataExist:
+                            // 単にデータを読めばいい状態になった
+                            return;
+                        case MessageStateRewind:
+                            // readerがrewindフラグ(≒MMFのファイル末尾)まで到達した = readerもwriterもファイル冒頭に戻る
+                            lock (_readLock)
+                            {
+                                if (_accessor == null)
+                                {
+                                    return;
+                                }
+                                _accessor.Write(0, MessageStateEmpty);
+                                _index = 0;
+                            }
+                            await Task.Delay(1, token);
+                            break;
+                        default:
+                            throw new InvalidOperationException("unsupported message state");
+                    }
                 }
             }
             
-            private void HandleReceivedMessage(ReadOnlyMemory<byte> message, int id, bool isReply)
+            private void HandleReceivedMessage(ReadOnlyMemory<byte> message, ushort queryId, bool isReply)
             {
                 //3パターンある
                 // - こちらが投げたQueryの返答が戻ってきた
@@ -142,15 +182,15 @@ namespace Baku.VMagicMirror.Mmf
                 // - Queryを受け取った
                 if (isReply)
                 {
-                    ReceiveQueryResponse?.Invoke((id, message));
+                    ReceiveQueryResponse?.Invoke((queryId, message));
                 }
-                else if (id == 0)
+                else if (queryId == 0)
                 {
                     ReceiveCommand?.Invoke(message);
                 }
                 else
                 {
-                    ReceiveQuery?.Invoke((id, message));
+                    ReceiveQuery?.Invoke((queryId, message));
                 }
             }
         }
--- VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Interprocess/MemoryMappedFile/MemoryMappedFileConnectWriter.cs
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Interprocess/MemoryMappedFile/MemoryMappedFileConnectWriter.cs
@@ -3,25 +3,41 @@
 using System;
 using System.Collections.Concurrent;
 using System.IO.MemoryMappedFiles;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Baku.VMagicMirror.Mmf
 {
+    // 基本的なデータ書き込みの流れ:
+    // - MMFの先頭から順にデータを書き込んでファイル領域を先に進む。これがQueueのように作用する
+    //   - このとき、チャンク終端の次のバイトには逐一 `0` を書く (readerが追い越して読み込むのを防ぐため)
+    // - チャンクの書き込みに十分な領域が余ってないのを検知したら、チャンクの代わりに `2` だけを書き込んだあと、ファイル冒頭に戻る
+    // - ファイルの冒頭が(reader側のフラグ書き換えによって)書き込み可になるまで待つ
+    // - 最初に戻る
+    
     public partial class MemoryMappedFileConnector
     {
         private sealed class InternalWriter
         {
-            // NOTE: ファイルはOpenExistingとかでも開くことがあり、直ちに開けるとは保証できないのでnullableになる
+            // NOTE: 呼び出し元の知識も混入しているが、
+            // 「Header + 8byte(=intデータ1つからなるコマンドのpayload) + 次の余り1byte」
+            // を分割せず書き込める領域もないのならRewindしたほうがよい…という考え方でこのしきい値にしている
+            private const int RewindMinIndex = (int)MemoryMappedFileCapacity - (MessageHeaderSize + 9);
+            
+            // NOTE: ファイルはOpenExistingとかでも開くことがあり、直ちに開けるとは保証できないのでnullable
             private MemoryMappedFile? _file;
             private MemoryMappedViewAccessor? _accessor;
-            private readonly object _senderLock = new();
+            private readonly object _writeLock = new();
             private readonly byte[] _writeBuffer = new byte[MemoryMappedFileCapacity];
             
             // 送りたいMessageの一覧。コマンド、クエリ、クエリのレスポンスを区別せず一列に並べる
             private readonly ConcurrentQueue<Message> _messages = new();
-
+            
+            // 次のチャンクを書き込む冒頭のファイル上のインデックス値。
+            // readerの読み込みを待っている場合、このindexが指している部分に書き込み済みフラグが既に立っている
+            // (& readerがそのフラグを消すのを待つ)事がある
+            private int _index;
+            
             private readonly object _lastMessageLock = new();
             private bool _lastMessageEnqueued;
             private bool LastMessageEnqueued
@@ -35,26 +51,14 @@ namespace Baku.VMagicMirror.Mmf
                     lock (_lastMessageLock) _lastMessageEnqueued = value;
                 }
             }
-            
-            private bool CanWriteMessage
-            {
-                get
-                {
-                    lock (_senderLock)
-                    {
-                        return
-                            _accessor != null &&
-                            _accessor.ReadByte(0) == MessageStateWriteReady;
-                    }
-                }
-            }
 
             public bool LastMessageSent { get; private set; }
             
+            // TODO: ファイル冒頭に戻るときだけTaskを実行し、それ以外は即時書き込みしていくように直したい
             // NOTE: Start / Stopがライフサイクルで1回ずつしか呼ばれない前提でこういう書き方にしてる
             public async Task RunAsync(MemoryMappedFile file, CancellationToken token)
             {
-                lock (_senderLock)
+                lock (_writeLock)
                 {
                     _file = file;
                     _accessor = file.CreateViewAccessor();
@@ -67,6 +71,12 @@ namespace Baku.VMagicMirror.Mmf
                         // 送るものが無いうちは単に待つ
                         if (!_messages.TryDequeue(out var msg))
                         {
+                            // NOTE: SendCommandで指定した最終メッセージが一瞬で消費されてしまうと後述のif文を通過しないので、ここでも判定する
+                            // TODO: タイミング問題の捌きがややこしいのでもうちょっとわかりやすくしたい && 大部分の処理を同期化することで大枠としてケアできると嬉しい
+                            if (LastMessageEnqueued)
+                            {
+                                return;
+                            }
                             await Task.Delay(1, token);
                             continue;
                         }
@@ -85,7 +95,7 @@ namespace Baku.VMagicMirror.Mmf
                 }
                 finally
                 {
-                    lock (_senderLock)
+                    lock (_writeLock)
                     {
                         _file?.Dispose();
                         _file = null;
@@ -101,7 +111,7 @@ namespace Baku.VMagicMirror.Mmf
                 {
                     return;
                 }
-                
+
                 _messages.Enqueue(Message.Command(content));
 
                 if (isLastMessage)
@@ -110,7 +120,7 @@ namespace Baku.VMagicMirror.Mmf
                 }
             }
 
-            public void SendQuery(int id, ReadOnlyMemory<byte> content)
+            public void SendQuery(ushort id, ReadOnlyMemory<byte> content)
             {
                 if (LastMessageEnqueued)
                 {
@@ -120,7 +130,7 @@ namespace Baku.VMagicMirror.Mmf
                 _messages.Enqueue(Message.Query(content, id));
             }
 
-            public void SendQueryResponse(int id, ReadOnlyMemory<byte> content)
+            public void SendQueryResponse(ushort id, ReadOnlyMemory<byte> content)
             {
                 if (LastMessageEnqueued)
                 {
@@ -138,10 +148,11 @@ namespace Baku.VMagicMirror.Mmf
                 var writeSize = 0;
                 while (!token.IsCancellationRequested && writeSize < data.Length)
                 {
-                    //書き込みOKになるまで待ち
-                    while (!CanWriteMessage)
+                    // 書き込みできない(ファイル末尾に到達した)場合だけ非同期処理に帰着して、ファイル冒頭に戻す。
+                    if (ShouldRewind())
                     {
-                        await Task.Delay(1, token);
+                        WriteRewindFlagAndResetIndex();
+                        await WaitRewindAsync(token);
                     }
 
                     // 書き込んだあと、正常に書き込めていればサイズを累積して続行
@@ -155,35 +166,93 @@ namespace Baku.VMagicMirror.Mmf
                 }
             }
 
+            private bool ShouldRewind()
+            {
+                lock (_writeLock)
+                {
+                    return _index > RewindMinIndex;
+                }
+            }
+
+            private void WriteRewindFlagAndResetIndex()
+            {
+                lock (_writeLock)
+                {
+                    if (_accessor == null)
+                    {
+                        return;
+                    }
+                    _accessor.Write(_index, MessageStateRewind);
+                    // NOTE: この時点ではMMFの冒頭に `1` (書き込み済みフラグ) が書かれているのが期待値。
+                    // で、それをreaderが `0` に書き換えるのを待つことになる
+                    _index = 0;
+                }
+            }
+
+            // MMFをファイルの末尾まで使い切ったときに呼び出すことで、readerがファイル冒頭への書き込みを許可するまで待機する
+            private async Task WaitRewindAsync(CancellationToken token)
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    byte state;
+                    lock (_writeLock)
+                    {
+                        if (_accessor == null)
+                        {
+                            return;
+                        }
+                        // 意味的には ReadByte(_index) でも正しいが、このメソッドを呼ぶとき常に _index = 0 なので定数で書いてる
+                        state = _accessor.ReadByte(0);
+                    }
+
+                    if (state == MessageStateEmpty)
+                    {
+                        return;
+                    }
+                    
+                    await Task.Delay(1, token);
+                }
+            }
+            
             // NOTE:
             //  - 戻り値はデータを書き込んだバイト数。
             //  - ただし、書き込みが失敗した場合には -1 を返す。-1 が戻った場合、それ以降は書き込みを試みないのが期待値
-            private int WriteMessage(Message msg, int offset)
+            private int WriteMessage(Message msg, int dataOffset)
             {
                 var data = msg.Body;
-                lock (_senderLock)
+                lock (_writeLock)
                 {
                     if (_accessor == null)
                     {
                         return -1;
                     }
 
-                    _accessor.Write(2, (short)(msg.IsReply ? 1 : 0));
-                    _accessor.Write(4, msg.Id);
-                    _accessor.Write(8, data.Length);
+                    _accessor.Write(_index + 1, msg.IsReply ? MessageTypeResponse : MessageTypeCommandOrQuery);
+                    _accessor.Write(_index + 2, msg.QueryId);
+                    _accessor.Write(_index + 4, data.Length);
 
-                    // データが乗り切らない場合は書けるとこまでにする。大多数のケースでは一回でメッセージ全部が書き切れるはず
-                    var writeLength = data.Length - offset > MaxMessageBodySize
-                        ? MaxMessageBodySize
-                        : data.Length - offset;
-                    _accessor.Write(12, writeLength);
+                    // NOTE:
+                    // - このmaxDataLengthが1以上になることは事前に ShouldResetToStart() を判定することで保証されている
+                    // - 必ず1バイト余らせるのはRewindフラグを常に明示的に立てるため (≒reader側はMMFのファイル全長に関知しないでもよい)
+                    var maxDataLength = (int)MemoryMappedFileCapacity - MessageHeaderSize - _index - 1;
+                    
+                    var writeLength = Math.Min(
+                        data.Length - dataOffset,
+                        maxDataLength
+                        );
+                    _accessor.Write(_index + 8, writeLength);
 
                     // _accessor.WriteArrayはSpanを直接受けないため、byte[]に書いてから書き込む
                     // NOTE: 書き込み側のメッセージを単に byte[] で受けるようにしてバッファを挟むのをやめてもよい
-                    data.Span[offset..(offset + writeLength)].CopyTo(_writeBuffer);
-                    _accessor.WriteArray(16, _writeBuffer, 0, writeLength);
+                    data.Span[dataOffset..(dataOffset + writeLength)].CopyTo(_writeBuffer);
+                    _accessor.WriteArray(_index + 12, _writeBuffer, 0, writeLength);
 
-                    _accessor.Write(0, (byte)MessageStateReadReady);
+                    var chunkStartIndex = _index;
+                    _index += MessageHeaderSize + writeLength;
+                    // readerが読み込みを追い越さないように、チャンクの次をあらかじめ未書き込み扱いにしておく。
+                    // この書き込みはMMFの生成直後には意味がなくて、MMFの末尾まで到達して二巡目をやっている(=汚い状態になったMMFを使っている)とき
+                    _accessor.Write(_index, MessageStateEmpty);
+                    _accessor.Write(chunkStartIndex, MessageStateDataExist);
                     return writeLength;
                 }
             }

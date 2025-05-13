10d533bd85f658d09f9b519428417e6c601b3096
Baku Dreameater <njslyr.trpg+harriet@gmail.com>
2025-05-13 16:42:44 +0900

    Add "?" mark + tooltip UI to provide description

---
+++ WPF/VMagicMirrorConfig/View/Code/Converter/StringEmptyToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Baku.VMagicMirrorConfig.View
+{
+    /// <summary> 空文字列ならVisible、そうでなければCollapsedにするコンバータ </summary>
+    public class StringEmptyToVisibilityConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value switch
+            {
+                string s => string.IsNullOrWhiteSpace(s) ? Visibility.Visible : Visibility.Collapsed,
+                _ => Visibility.Collapsed,
+            };
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => Binding.DoNothing;
+    }
+}

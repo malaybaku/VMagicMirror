90fb4eaf02ecaa2714dbce63a178e9b4394847c0
Baku Dreameater <njslyr.trpg+harriet@gmail.com>
2025-05-21 12:09:54 +0900

    prevent to load script out of Buddy directory
    this settings also excludes #load directives in DefaultBuddy's buddy

--- VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Buddy/IgnoreFileDefinedScriptSourceResolver.cs
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Buddy/IgnoreFileDefinedScriptSourceResolver.cs
@@ -53,12 +53,22 @@ namespace Baku.VMagicMirror.Buddy
                 return true;
             }
 
-            // NOTE: こっちのif文はデフォルトサブキャラが #load したときのみ影響する。普通のサブキャラでの発生はほぼあり得ない
-            var exeDir = Path.GetDirectoryName(Application.dataPath)?.Replace('/', '\\');
-            if (!string.IsNullOrEmpty(exeDir) && fullPath.StartsWith(exeDir, StringComparison.OrdinalIgnoreCase))
+            // NOTE: 明らかにサブキャラと関係ないフォルダを見に行こうとするのを全体的に禁止する。
+            // この処置のpositiveな副作用として、デフォルトサブキャラ(VMM.exe付近のフォルダに存在)の #load は全て無視される
+            if (!fullPath.StartsWith(SpecialFiles.BuddyRootDirectory))
             {
                 return true;
             }
+            
+            // NOTEのNOTE: ひとつ手前の条件によって下記のガードが冗長になるので、省いている
+            // NOTE: VMagicMirror.exeのあるフォルダ以下の #load を一律で禁止する。
+            // 普通にスクリプトを書いて本処理が適用されるのはデフォルトサブキャラのみである
+            // (※デフォルトサブキャラで #load が使いたくなったら書き方を考える必要がある)
+            // var exeDir = Path.GetDirectoryName(Application.dataPath)?.Replace('/', '\\');
+            // if (!string.IsNullOrEmpty(exeDir) && fullPath.StartsWith(exeDir, StringComparison.OrdinalIgnoreCase))
+            // {
+            //     return true;
+            // }
 
             return false;
         }

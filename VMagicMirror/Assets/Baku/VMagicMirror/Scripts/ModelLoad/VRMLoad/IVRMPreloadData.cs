3ab8fcad735e10aa2d6c7bdf8c662c5fa7238bf0
Baku Dreameater <njslyr.trpg+harriet@gmail.com>
2025-06-04 11:37:02 +0900

    Define preload VRM

---
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/ModelLoad/VRMLoad/IVRMPreloadData.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Baku.VMagicMirror
+{
+    /// <summary>
+    /// MainViewerシーンの実行時点でロード対象のモデルが判別出来ている場合にそのデータを保持するインターフェース。
+    /// </summary>
+    /// <remarks>
+    /// アプリケーションにデフォルトモデル等を定義できる場合のI/Fとして検証も兼ねて追加しています。
+    /// </remarks>
+    public interface IVRMPreloadData
+    {
+        public bool HasData { get; }
+
+        /// <summary> VRMのバイナリです。 </summary>
+        public byte[] GetData();
+
+        /// <summary>
+        /// 対象のVRMが1.0かどうかを取得します。不明な場合、nullを返します。
+        /// </summary>
+        /// <returns></returns>
+        public bool? IsVrm10();
+    }
+
+    /// <summary>
+    /// プリロードするデータが特にないようなVRMPreloadDataの実装。
+    /// </summary>
+    /// <remarks>
+    /// 特にIVRMPreloadDataを使わないときの実体としてコレを使う
+    /// </remarks>
+    public class EmptyVRMPreloadData : IVRMPreloadData
+    {
+        public bool HasData => false;
+        public byte[] GetData() => Array.Empty<byte>();
+        public bool? IsVrm10() => null;
+    }
+}

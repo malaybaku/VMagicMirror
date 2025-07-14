935dd2ed1d3891c2d7572fce103d6c63f4638804
Baku Dreameater <njslyr.trpg+harriet@gmail.com>
2025-07-14 21:54:13 +0900

    Fix binding

--- VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Installer/VMCPInstaller.cs
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Installer/VMCPInstaller.cs
@@ -24,7 +24,7 @@ namespace Baku.VMagicMirror
             Container.BindInterfacesAndSelfTo<VMCPBasedFingerSetter>().AsSingle();
             Container.BindInterfacesAndSelfTo<VMCPBlendShape>().AsSingle();
             Container.BindInterfacesAndSelfTo<VMCPActiveness>().AsSingle();
-            Container.BindInterfacesTo<VMCPReceiver>().AsSingle();
+            Container.BindInterfacesAndSelfTo<VMCPReceiver>().AsSingle();
             Container.Bind<VMCPFingerController>().FromNewComponentOnNewGameObject().AsSingle();
             Container.Bind<VMCPNaiveBoneTransfer>().FromNewComponentOnNewGameObject().AsSingle();
             

ab2a98e26bc43da315b45139f5e7ac918519c88d
Baku Dreameater <njslyr.trpg+harriet@gmail.com>
2025-08-10 15:02:07 +0900

    fix Rx extension about how to ignore latest value

--- VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Utils/UniRxExtension.cs
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Utils/UniRxExtension.cs
@@ -1,14 +1,13 @@
-using System;
 using R3;
 
 namespace Baku.VMagicMirror
 {
     public static class UniRxExtension
     {
-        public static IObservable<Unit> AsUnitWithoutLatest<T>(this ReactiveProperty<T> rp)
-            => rp.SkipLatestValueOnSubscribe().AsUnitObservable();
+        public static Observable<Unit> AsUnitWithoutLatest<T>(this ReactiveProperty<T> rp)
+            => rp.AsUnitObservable();
 
-        public static IObservable<Unit> AsUnitWithoutLatest<T>(this ReadOnlyReactiveProperty<T> rp)
-            => rp.SkipLatestValueOnSubscribe().AsUnitObservable();
+        public static Observable<Unit> AsUnitWithoutLatest<T>(this ReadOnlyReactiveProperty<T> rp)
+            => rp.AsUnitObservable();
     }
 }

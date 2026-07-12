353ad2bc442a44251b80b00df8c13c59fbe6378f
Baku Dreameater <njslyr.trpg+harriet@gmail.com>
2026-07-12 16:26:41 +0900

    Show crash mode guidance in tooltip

--- WPF/VMagicMirrorConfig/View/ControlPanel/HandTrackingPanel.xaml.cs
+++ WPF/VMagicMirrorConfig/View/ControlPanel/HandTrackingPanel.xaml.cs
@@ -8,5 +8,24 @@ namespace Baku.VMagicMirrorConfig.View
         {
             InitializeComponent();
         }
+
+        private async void OnCrashPreventionHelpButtonClicked(object? sender, System.Windows.RoutedEventArgs e)
+        {
+            // ボタンクリック時に閉じるToolTipを、クリック操作でも確認できるよう開き直す
+            if (sender is Button { ToolTip: ToolTip tooltip })
+            {
+                await System.Threading.Tasks.Task.Delay(16);
+                tooltip.PlacementTarget = sender as Button;
+                tooltip.IsOpen = true;
+            }
+        }
+
+        private void OnCrashPreventionHelpButtonMouseLeave(object? sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (sender is Button { ToolTip: ToolTip tooltip })
+            {
+                tooltip.IsOpen = false;
+            }
+        }
     }
 }
